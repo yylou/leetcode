32549252e4308ab448fa13e63ebfb6cdcea057f3
Yuan-Yao Lou <yyloumike@gmail.com>
2021-02-25 22:12:20 +0800

    LeetCode problem 80 (Update)

--- 0080-remove-duplicates-from-sorted-array-II/solution.py
+++ 0080-remove-duplicates-from-sorted-array-II/solution.py
@@ -1,4 +1,5 @@
-##  related problem: 26. Remove Duplicates from Sorted Array. For this problem, only need to add one counter and an if-else statement
+##  Related problem: 26. Remove Duplicates from Sorted Array
+##  For this problem, only need to add one counter and additional if-else statements
 class Solution(object):
     def removeDuplicates(self, nums):
         """
@@ -8,27 +9,27 @@ class Solution(object):
 
         length = len( nums )
 
-        ##  edge case handling
+        ##  (edge case)
         if length == 0 : return 0
         if length == 1 : return 1
 
         counter = 0
-        ret_value = 0
+        retVal = 0
 
         for i in range( 1, length ) :
-            if nums[i] == nums[ret_value] :
+            if nums[i] == nums[retVal] :
                 counter += 1
 
                 ##  not exceeding 2 times, move to the next element but CANNOT renew counter
-                if counter < 2 : ret_value += 1
+                if counter < 2 : retVal += 1
 
             ##  uneqal condition: renew counter and move to the next element concurrently (ret_value and i)
             else :
                 counter = 0
-                ret_value += 1
+                retVal += 1
 
             ##  assign element by two pointers: ret_value and i
-            nums[ret_value] = nums[i]
+            nums[retVal] = nums[i]
 
 
-        return ret_value + 1
+        return retVal + 1
