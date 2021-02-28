add34b4ea2ee9762b5d151f21cad0e62e3136865
Yuan-Yao Lou <yyloumike@gmail.com>
2021-02-28 19:00:36 +0800

    LeetCode problem 6

---
+++ 0006-zigzag-conversion/solution.py
@@ -0,0 +1,33 @@
+##  visit by row
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+
+        length = len( s )
+
+        ##  (edge case) numRows = 1, row > length of string
+        if numRows == 1 : return s
+        if numRows >= length : return s
+
+        ##  calculate the divide unit
+        gap = numRows - 2
+        cycle = numRows + gap
+
+        record = ''
+
+        for i in range( numRows ) :
+            j = 0
+            while j+i < length :
+                record += s[j + i]
+
+                ##  INNER elements
+                if i != 0 and i != numRows - 1 and j + cycle -i < length :
+                    record += s[j + cycle - i]
+
+                j += cycle
+
+        return record
