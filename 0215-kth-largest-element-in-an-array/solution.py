42561e8c31bc66dd65fa71703bbf14a9a1aed2cb
Yuan-Yao Lou <yyloumike@gmail.com>
2021-03-30 00:50:05 +0800

    LeetCode problem 215

--- 0215-kth-largest-element-in-an-array/solution.py
+++ 0215-kth-largest-element-in-an-array/solution.py
@@ -10,51 +10,7 @@ class Solution(object):
         if k == 1: return max(nums)
 
 
-        #:  Solution (1) sort
-        #:  - time complexity: O(nlogn)
-        #:  - space complexity: O(1)
-        def mergeSort( array ):
-            if len(array) > 1:
-                mid = len(array) // 2
-                arrayL, arrayR = array[:mid], array[mid:]
-
-                mergeSort(arrayL)
-                mergeSort(arrayR)
-
-                lenL, lenR = len(arrayL), len(arrayR)
-
-                pL = pR = curP = 0
-
-                #: merge two sorted lists
-                while pL < lenL and pR < lenR:
-                    if arrayL[pL] < arrayR[pR]:
-                        array[curP] = arrayL[pL]
-                        pL += 1
-                    else:
-                        array[curP] = arrayR[pR]
-                        pR += 1
-                    curP += 1
-
-                #: checking remaining elements
-                while pL < lenL:
-                    array[curP] = arrayL[pL]
-                    pL += 1
-                    curP += 1
-
-                while pR < lenR:
-                    array[curP] = arrayR[pR]
-                    pR += 1
-                    curP += 1
-
-        # mergeSort( nums )
-        # nums.sort()
-        # return nums[len(nums)-k]
-
-
-        # ======================================================================== #
-
-
-        #:  Solution (2) "Hoare's selection algorithm"
+        #:  Solution (1) "Hoare's selection algorithm"
         #:  - time complexity: O(n)
         #:  - space complexity: O(1)
         def partition( left, right, index ):
@@ -79,8 +35,8 @@ class Solution(object):
         def quickSelect( left, right, k_smallest ):
             if left == right: return nums[left]
 
-            #:  randomly assign pivot and do partition
-            index = partition( left, right, random.randint( left, right ) )
+            #:  assign pivot by the mid point and do partition
+            index = partition( left, right, left + (right-left)/2 )
 
             if index == k_smallest: return nums[index]
             elif index > k_smallest: return quickSelect( left, index-1, k_smallest )
@@ -89,6 +45,50 @@ class Solution(object):
 
         n = len(nums)
         return quickSelect( 0, n-1, n-k )  #:  convert to k-smallest problem
+
+
+        # ======================================================================== #
+
+
+        #:  Solution (2) sort
+        #:  - time complexity: O(nlogn)
+        #:  - space complexity: O(1)
+        def mergeSort( array ):
+            if len(array) > 1:
+                mid = len(array) // 2
+                arrayL, arrayR = array[:mid], array[mid:]
+
+                mergeSort(arrayL)
+                mergeSort(arrayR)
+
+                lenL, lenR = len(arrayL), len(arrayR)
+
+                pL = pR = curP = 0
+
+                #:  merge two sorted lists
+                while pL < lenL and pR < lenR:
+                    if arrayL[pL] < arrayR[pR]:
+                        array[curP] = arrayL[pL]
+                        pL += 1
+                    else:
+                        array[curP] = arrayR[pR]
+                        pR += 1
+                    curP += 1
+
+                #:  checking remaining elements
+                while pL < lenL:
+                    array[curP] = arrayL[pL]
+                    pL += 1
+                    curP += 1
+
+                while pR < lenR:
+                    array[curP] = arrayR[pR]
+                    pR += 1
+                    curP += 1
+
+        # mergeSort( nums )
+        nums.sort()
+        return nums[len(nums)-k]
 
 
         # ======================================================================== #
