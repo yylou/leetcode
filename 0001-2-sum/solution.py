44647368d0d34d80619bbac8c132dbbf27e011b2
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-01 23:19:54 +0800

    Update solution.py

--- 0001-2-sum/solution.py
+++ 0001-2-sum/solution.py
@@ -1,17 +1,36 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        hashTable = {}
-
-        for i in range( len( nums ) ) :
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashTable = dict()
+        
+        for i in range( len(nums) ):
             remain = target - nums[i]
-
-            ##  since there is only one exact solution, it has no problem to overwrite the record
-            if remain not in hashTable :
-                hashTable[nums[i]] = i
-            else :
+            
+            if remain in hashTable:
                 return [i, hashTable[remain]]
+            
+            #:  Since there is only one exact solution, it has no problem to overwrite the record
+            hashTable[nums[i]] = i
+            
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**  
+     * @time  : O(n)
+     * @space : O(n)
+     */
+    
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for( int i = 0; i < nums.length; i++ ) {
+            int remain = target - nums[i];
+            if( map.containsKey(potentialMatch) ) {
+                return new int[] {i, map.get(potentialMatch)};
+            }
+            map.put(nums[i], i);
+        }
+    }
+}
+==================================================================================================
+'''
