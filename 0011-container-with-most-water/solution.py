f071428bf318a3a5da27f7b614907405af35647e
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-06 21:10:39 +0800

    Update solution.py

--- 0011-container-with-most-water/solution.py
+++ 0011-container-with-most-water/solution.py
@@ -1,27 +1,77 @@
-class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-
-        length = len( height )
-
-        front, back = 0, length -1
-        ret_value = 0
-
-        ##  Solution: Two pointers with reduced calculation
-
-        ##  searching for the HEIGHTest elements costs, instead, WIDTH is also an equally important metric.
-        ##  Therefore, using two pointers starting from the right-most and left-most respectively (WIDEST)
-        while front < back :
-            if height[front] < height[back] :
-                area = ( back-front ) * height[front]
-                front += 1
-            else :
-                area = ( back-front ) * height[back]
-                back -= 1
-
-            if area > ret_value : ret_value = area
-
-        return ret_value
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        #:  (edge case) only 2 elements
+        if len( height ) == 2: return min( height )
+        
+        # ==================================================
+        #  Array + Two Pointer                             =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        area = 0
+        l, r = 0, len(height)-1
+        
+        #:  start from both-side (due to the LARGEST gap)
+        #:  move the pointer with SHORTER height but SKIP calculating area when height is ZERO
+        while r > l:
+            if height[r] > height[l]:
+                if height[l] == 0: 
+                    l += 1
+                    continue
+                    
+                tmp = height[l] * ( r-l )
+                l += 1
+                
+            else:
+                if height[r] == 0:
+                    r -= 1
+                    continue
+                
+                tmp = height[r] * (r-l)
+                r -= 1
+                
+            if tmp > area: area = tmp
+                
+        return area
+    
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    public int maxArea(int[] height) {
+        if( height.length == 2 ) return Math.min( height[0], height[1] );
+        
+        int l = 0, r = height.length - 1;
+        int area = 0;
+            
+        while( l < r ){
+            int tmp = 0;
+            
+            if( height[l] < height[r] ){
+                if( height[l] == 0 ){
+                    l += 1;
+                    continue;
+                }
+                
+                tmp = height[l] * ( r-l );
+                l += 1;
+                
+            } else{
+                if( height[r] == 0 ){
+                    r -= 1;
+                    continue;
+                }
+                
+                tmp = height[r] * ( r-l );
+                r -= 1;
+            }
+            
+            if( tmp > area ) area = tmp;
+        }
+        
+        return area;
+    }
+}
+==================================================================================================
+'''
