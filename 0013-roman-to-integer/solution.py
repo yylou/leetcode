2d8f976cfc5fcbd93c54bd3ef25129f9ae8d1df5
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-07 13:36:47 +0800

    Update solution.py

--- 0013-roman-to-integer/solution.py
+++ 0013-roman-to-integer/solution.py
@@ -1,33 +1,52 @@
-##  mathematical solution
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        map_table = { 'I'  :    1,
-                      'V'  :    5,
-                      'X'  :   10,
-                      'L'  :   50,
-                      'C'  :  100,
-                      'D'  :  500,
-                      'M'  : 1000  }
-
-        ##  (edge case) input already in the table
-        if len( s ) == 1 : return map_table[s]
-
-        retVal = 0
-        curType = 1000
-
-        for char in s :
-            value = map_table[char]
-
-            retVal += value
-
-            ##  if previous char mapped to smaller value (compared with current char)
-            ##  subtract the mapped value * 2
-            if value > curType : retVal -= curType * 2
-
-            curType = value
-
-        return retVal
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        RtoI = {
+            "I":  1,
+            "IV": 4,
+            "V":  5,
+            "IX": 9,
+            "X":  10,
+            "XL": 40, 
+            "L":  50,
+            "XC": 90,
+            "C":  100,
+            "CD": 400,
+            "D":  500,
+            "CM": 900,
+            "M":  1000,
+        }
+        
+        # ==================================================
+        #  String + Math (Left to Right)                   =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        ans   = 0
+        index = 0
+        
+        while index < len(s):
+            if index+1 < len(s) and s[index:index+2] in RtoI:
+                ans += RtoI[s[index:index+2]]
+                index += 2
+            
+            else:
+                ans += RtoI[s[index]]
+                index += 1
+                
+        return ans
+    
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
+    public int romanToInt(String s) {
+        
+    }
+}
+==================================================================================================
+'''
