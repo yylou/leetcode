a787548ebc74f06858757c7e768ad7a314c13705
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-07 14:19:15 +0800

    Update solution.py

--- 0014-longest-common-prefix/solution.py
+++ 0014-longest-common-prefix/solution.py
@@ -1,59 +1,88 @@
-##  Trie solution, related to LeetCode problem - 208. Implement Trie (Prefix Tree)
 class Trie:
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.root = {}
-
-
+        
     def insert(self, word):
-        """
-        Inserts a word into the trie.
-        :type word: str
-        :rtype: None
-        """
         curNode = self.root
-
+        
         for char in word:
-            ##  check the existence of CHAR, if not found, create a new hash_table to record CHAR
-            if char not in curNode: curNode[char] = dict()
-
+            if char not in curNode: curNode[char] = {}
+            
             curNode = curNode[char]
-
-        curNode['end_of_word'] = True
-
-
+            
+        curNode['#'] = True
+        
     def longestCommonPrefix(self):
-        ret_value = ''
+        prefix  = ''
         curNode = self.root
-
+        
         while True:
-            if len( curNode ) == 1 and 'end_of_word' not in curNode : ret_value += curNode.keys()[0]
-            else : return ret_value
+            if len(curNode) == 1 and '#' not in curNode: 
+                curChar = list( curNode.keys() )[0]
+                prefix += curChar
+                curNode = curNode[curChar]
+            else: 
+                return prefix
 
-            curNode = curNode[curNode.keys()[0]]
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        #:  (edge case)
+        if not strs: return ''
+        if len(strs) == 1: return strs[0]
+        
+        # ==================================================
+        #  Trie                                            =
+        # ==================================================
+        # n = number of all characters in the array
+        #
+        # time  : O(n)
+        # space : O(n) 
 
-        return ret_value
+        trie = Trie()
+        
+        for word in strs: trie.insert(word)
+            
+        return trie.longestCommonPrefix()
+    
+        '''
+        
+        # ==================================================
+        #  Array + Sort                                    =
+        # ==================================================
+        # time  : O(nlog(n))
+        # space : O(1)
+        
+        strs.sort()
+        
+        shortStr = strs[0]
+        longStr  = strs[-1]
+        prefix   = ''
+        
+        for i in range( len(shortStr) ):
+            if shortStr[i] != longStr[i]: return prefix
+            else: prefix += shortStr[i]
+                
+        return prefix
+        
+        
+        # ==================================================
+        #  Python Built-in Functions                       =
+        # ==================================================
+        os.path.commonprefix(strs)
+        
+        '''
 
-
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-
-        length = len( strs )
-
-        ##  edge case handling
-        if not strs : return ''
-        if strs[0] == '' : return ''
-        if length == 1 : return strs[0]
-
-        trie_data = Trie()
-
-        ##  insert each string into Trie data structure
-        for i in range( length ) : trie_data.insert( strs[i] )
-
-        return trie_data.longestCommonPrefix()
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O()
+     * @space : O()
+     */
+    public String longestCommonPrefix(String[] strs) {
+        
+    }
+}
+==================================================================================================
+'''
