ca0f5f11b912237c678fe310209ee5678dafa377
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-07 09:47:23 +0800

    Update solution.py

--- 0012-integer-to-roman/solution.py
+++ 0012-integer-to-roman/solution.py
@@ -1,38 +1,45 @@
-##  mathematical solution
-class Solution(object):
-    def intToRoman(self, num):
-        """
-        :type num: int
-        :rtype: str
-        """
-        map_table = {1:    'I',
-                     5:    'V',
-                     10:   'X',
-                     50:   'L',
-                     100:  'C',
-                     500:  'D',
-                     1000: 'M'}
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        digits = [(1000, "M"),  (900, "CM"), 
+                  (500, "D"),   (400, "CD"), 
+                  (100, "C"),   (90, "XC"), 
+                  (50, "L"),    (40, "XL"), 
+                  (10, "X"),    (9, "IX"), 
+                  (5, "V"),     (4, "IV"), 
+                  (1, "I")]
+        
+        # ==================================================
+        #  Math + Greedy                                   =
+        # ==================================================
+        # time  : O(1)
+        # space : O(1)
+        
+        ret = ''
+        
+        for value, symbol in digits:
+            if num == 0: break
+            
+            if num == value:
+                ret += symbol
+                break
+                
+            count, num = divmod( num, value )
+            ret += symbol * count
+            
+        return ret
+    
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**  
+     * @time  : O(1)
+     * @space : O(1)
+     */
 
-        retVal = ""
-
-        iterative = sorted( map_table.keys(), reverse=True )
-
-        for key, value in enumerate( iterative ):
-            if num >= value:
-                scale, strNum = int( num / value ), str( num )
-
-                ##  4: current char - previous char (e.g., 40: 50 - 10 = XL)
-                if strNum[0] == '4' :
-                    retVal += map_table[value] + map_table[iterative[key-1]]
-                    num -= scale * value
-
-                ##  9: next char - previous char (e.g., 900: 1000 - 100 = CM)
-                elif strNum[0] == '9' :
-                    retVal += map_table[iterative[key+1]] + map_table[iterative[key-1]]
-                    num -= 9 * 10 ** ( len( strNum ) -1 )
-
-                else:
-                    retVal += map_table[value] * scale
-                    num -= value * scale
-
-        return retVal
+    public String intToRoman(int num) {
+        
+    }
+}
+==================================================================================================
+'''
