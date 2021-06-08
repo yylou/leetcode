3a2c0bd96a903cb24b211b2a317b85508a3d535d
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-08 23:32:35 +0800

    Update solution.py

--- 0021-merge-two-sorted-lists/solution.py
+++ 0021-merge-two-sorted-lists/solution.py
@@ -1,48 +1,73 @@
-##  Definition for singly-linked list.
-# class ListNode(object):
+#: Definition for singly-linked list.
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 
-##  two pointer solution: moving the pointer of the linked list with smaller starting value, do comparison and node insertion
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        #:  (edge case)
+        if not l1 and l2: return l2
+        if not l2 and l1: return l1
+        if not l1 and not l2: return None
+        
+        # ==================================================
+        #  Linked List + Two Pointers                      =
+        # ==================================================
+        # n is the length of l1, and m is the length of l2
+        # time  : O(n+m), 
+        # space : O(1)
+            
+        ret = cur = ListNode(0)
+        
+        #:  keep iterating both lists until either one meets the end
+        #:  append the one with SMALLER node and move it to the next
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+                
+            else:
+                cur.next = l2
+                l2 = l2.next
+                
+            cur = cur.next
+            
+        #:  append the reamining linked lists, either l1 or l2
+        cur.next = l1 or l2
+            
+        return ret.next
+    
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n+m)
+     * @space : O(1)
+     */
 
-        ##  (edge case)
-        if not l1 and not l2 : return None
-        if not l1 and     l2 : return l2
-        if     l1 and not l2 : return l1
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode ret = new ListNode(0);
 
-        ##  compare the first element to choose the SMALLER one to be the GOLDEN (headNode)
-        if l1.val > l2.val :
-            curNode_1 = l2
-            curNode_2 = l1
+        ListNode cur = ret;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+                
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            
+            cur = cur.next;
+        }
 
-        else :
-            curNode_1 = l1
-            curNode_2 = l2
+        cur.next = l1 == null ? l2 : l1;
 
-        headNode = curNode_1
-
-        ##  loop through the non-GOLDEN linked list to do node insertion
-        while curNode_2 :
-            ##  reaching the end of GOLDEN linked list, append the rest and return
-            if curNode_1.next == None :
-                curNode_1.next = curNode_2
-                return headNode
-
-            ##  do numerical comparison and insert the node
-            elif curNode_2.val >= curNode_1.val and curNode_2.val < curNode_1.next.val :
-                curNode_1.next = ListNode( curNode_2.val, curNode_1.next )
-                curNode_2 = curNode_2.next
-
-            ##  the value of target node is smaller, move to the next node of GOLDEN list
-            else :
-                curNode_1 = curNode_1.next
-
-        return headNode
+        return ret.next;
+    }
+}
+==================================================================================================
+'''
