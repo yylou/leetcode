a83c0ea02b32b95aacf994b4f2eae8a58b2651bf
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-10 12:47:48 +0800

    Update solution.py

--- 0026-remove-duplicates-from-sorted-array/solution.py
+++ 0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,26 +1,49 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        #:  (edge case)
+        if len(nums) == 0 or len(nums) == 1: return len(nums)
+        
+        # ==================================================
+        #  Array + Two Pointer                             =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        slowP, fastP = 0, 0
+        
+        while fastP < len(nums):
+            if nums[fastP] != nums[slowP]:
+                slowP += 1
+                nums[slowP] = nums[fastP]
+                
+            fastP += 1
+                
+        return slowP+1
+    
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
 
-        ##  (edge case) empty list of integers
-        if not nums : return 0
-
-        length = len( nums )
-
-        if length == 1 : return 1
-
-        ##  using this index to flag the last unique element (also the index to put new integer)
-        uniqueIndex = 0
-
-        for i in range( length ) :
-            ##  (1) assign the unique element to the array by the current index
-            ##      since the system checks the answer by printing via the return index, pop or delete is not necessary
-            ##  (2) skip the duplicate element and the next iteration would overwrite the one
-            if nums[i] != nums[uniqueIndex] :
-                uniqueIndex += 1
-                nums[uniqueIndex] = nums[i]
-
-        return uniqueIndex + 1
+    public int removeDuplicates(int[] nums) {
+        if( nums.length == 0 || nums.length == 1 ) return nums.length;
+        
+        int slowP = 0, fastP = 0;
+        
+        while( fastP < nums.length ){
+            if( nums[slowP] != nums[fastP] ){
+                nums[++slowP] = nums[fastP];
+            }
+            
+            fastP++;
+        }
+        
+        return ++slowP;
+    }
+}
+==================================================================================================
+'''
