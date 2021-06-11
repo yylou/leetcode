2f15b8e03203b8fb5945732c9d11388388033b76
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-11 16:52:37 +0800

    Update solution.py

--- 0053-maximum-subarray/solution.py
+++ 0053-maximum-subarray/solution.py
@@ -1,53 +1,60 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-
-        ##  (edge case) length == 1
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #: (edge case)
         if len(nums) == 1: return nums[0]
-
-
-        ##  Solution (1) Greedy
-        localMax = globalMax = nums[0]
-        for i in xrange( 1, len(nums) ):
+        
+        # ==================================================
+        #  Array + Dynamic Programming + Greedy            =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+        
+        dp = [None] * len(nums)
+        dp[0] = nums[0]
+        
+        for i in range(1, len(nums)):
             ##  to determine local MAX, choose between accumulating or restart at the next number
             ##  - (example) [-2, 1, 2], it is clear that sum of (-2+1) is smaller than '1'
             ##                          so restart at '1' would be the next action
+            dp[i] = max(nums[i], dp[i-1]+nums[i])
+            
+        return max(dp)
+    
+        '''
+        # ==================================================
+        #  Array + Dynamic Programming + Greedy            =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        localMax = globalMax = nums[0]
+        
+        for i in xrange( 1, len(nums) ):
             localMax = max( nums[i], localMax + nums[i] )
-
             globalMax = max( localMax, globalMax )
 
         return globalMax
+        '''
+        
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
 
-
-        # ======================================================================================= #
-
-        ##  Solution (2) Divide and Conquer (follow LeetCode Solution)
-        mid = len(nums) // 2
-
-        leftSum  = self.maxSubArray( nums[:mid] )
-        rightSum = self.maxSubArray( nums[mid:] )
-        midSum   = self.crossSum( nums, mid )
-
-        return max( leftSum, rightSum, midSum )
-
-
-    def crossSum(self, nums, mid):
-        leftSum = rightSum = float( '-inf' )
-        tmpSum = 0
-
-        ##  (1) Left Part
-        for i in xrange( mid-1, -1, -1 ):
-            tmpSum += nums[i]
-            leftSum = max( tmpSum, leftSum )
-
-        tmpSum = 0
-
-        ##  (2) Right Part
-        for i in xrange( mid, len(nums) ):
-            tmpSum += nums[i]
-            rightSum = max( tmpSum, rightSum )
-
-        return leftSum + rightSum
+    public int maxSubArray(int[] nums) {
+        int localMax = nums[0], globalMax = nums[0];
+        
+        for( int i=1 ; i<nums.length ; i++){
+            localMax  = Math.max(nums[i], localMax + nums[i]);
+            globalMax = Math.max(localMax, globalMax);
+        }
+        
+        return globalMax;
+    }
+}
+==================================================================================================
+'''
