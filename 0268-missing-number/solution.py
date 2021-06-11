68082709d98ac9fd71c93cf12fa3ddb3df1e8db2
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-12 00:40:38 +0800

    Update solution.py

--- 0268-missing-number/solution.py
+++ 0268-missing-number/solution.py
@@ -1,29 +1,65 @@
-class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        ##  INTEGER RANGE = [0,n]
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        # ==================================================
+        #  Math                                            =
+        # ==================================================
+        # time  : O(n) for SUM operation
+        # space : O(1)
+        
+        total = sum(nums)
+        expectSum = len(nums)*(len(nums) + 1) // 2
+        return expectSum - total
+    
+        '''
+        # ==================================================
+        #  Bit Manipulation                                =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        #
+        # missing = 4 ∧ (0∧0) ∧ (1∧1) ∧ (2∧3) ∧ (3∧4)
+        #         = (4∧4) ∧ (0∧0) ∧ (1∧1) ∧ (3∧3) ∧ 2
+        #         = 0 ∧ 0 ∧ 0 ∧ 0 ∧ 2
+        #         = 2
+        
+        ans = len(nums)
+        
+        for i in range(ans):
+            ans ^= i ^ nums[i]
+            
+        return ans
+        
+        # ==================================================
+        #  Array                                           =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+        
+        visited = [0] * (len(nums) + 1)
+        
+        for element in nums: visited[element] = 1
+            
+        for i in range(len(visited)):
+            if not visited[i]: return i
+        '''
+        
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
 
-        ##  Solution (1) mathematical
-        ##  - expected sum of list of positive integers with legnth 4: (4*5)/2 = 10
-        ##  - test case: [1,3,2,4] (sum = 10, missing 0), [1,0,3,2] (sum = 6, missing 4)
-        ##
-        ##  - time complexity: O(n), due to SUM operation
-        ##  - space complexity: O(1)
-        curSum = sum( nums )
-        return len(nums)*(len(nums)+1)//2 - curSum
-
-        # ======================================================================================== #
-
-
-        ##  Solution (2) using set to record, and iterate from 0 to length to find the missing number
-        ##  - time complexity: O(n)
-        ##  - space complexity: O(n)
-        record = set(nums)
-
-        for i in range( len(record) ):
-            if i not in record: return i
-
-        return len(record)
+    public int missingNumber(int[] nums) {
+        int total = 0;
+        for(int num: nums) total += num;
+        int expectedSum = nums.length * (nums.length + 1) / 2;
+        
+        return expectedSum - total;
+    }
+}
+==================================================================================================
+'''
