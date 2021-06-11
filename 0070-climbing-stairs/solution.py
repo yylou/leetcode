c4e91514175112c350e2862734913d5cd5335c4d
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-11 18:38:52 +0800

    Update solution.py

--- 0070-climbing-stairs/solution.py
+++ 0070-climbing-stairs/solution.py
@@ -1,13 +1,57 @@
-class Solution(object):
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        ##  (edge case)
-        if n == 1 or n == 2: return n
-
-        first, second = 1, 2
-        for i in xrange( 3, n+1 ):
-            first, second = second, first + second
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        #: (base case) <4: 1, 2, 3
+        if n < 4: return n
+        
+        # ==================================================
+        #  Dynamic Programming                             =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+        
+        dp = [1, 2, 3] + [None] * (n-3)
+        for i in range( 3, n ):
+            dp[i] = dp[i-1] + dp[i-2]
+            
+        return dp[-1]
+    
+        '''
+        # ==================================================
+        #  Dynamic Programming                             =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        first, second = 2, 3
+        for i in range( 3, n ):
+            tmp = first
+            first = second
+            second += tmp
+            
         return second
+        '''
+        
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
+
+    public int climbStairs(int n) {
+        if( n < 4 ) return n;
+        
+        int first = 2, second = 3;
+        for( int i=3 ; i<n ; i++ ){
+            int tmp = first;
+            first = second;
+            second += tmp;
+        }
+        
+        return second;
+    }
+}
+==================================================================================================
+'''
