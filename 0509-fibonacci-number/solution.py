5a20e7331d5aae8e662b2879de41b676d73b6d1b
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-11 18:54:40 +0800

    Update solution.py

--- 0509-fibonacci-number/solution.py
+++ 0509-fibonacci-number/solution.py
@@ -1,64 +1,45 @@
-class Solution(object):
-    def fib(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-
-        if n == 0 : return 0
-        if n == 1 or n == 2 : return 1
-
-        ##  Dynamic Programming (bottom-up) Time: O(N)
-        ##  - Only record 2 of the previous results, Space: O(1)
-        pre = 1
-        cur = 1
-        for i in range( 3, n+1 ) :
-            sum = pre + cur
-            pre = cur
-            cur = sum
-        return cur
-
-        # ====================================================== #
-
-        ##  Brute Force Solution: O(2^N)
-        return self.fib(n-1) + self.fib(n-2)
-
+class Solution:
+    def fib(self, n: int) -> int:
+        #: (base case)
+        if n == 0 or n == 1: return n
+        
+        # ==================================================
+        #  Dynamic Programming                             =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        first, second = 0, 1
+        
+        for i in range( n - 1 ):
+            tmp = first
+            first = second
+            second += tmp
+            
+        return second
+    
 '''
-# =====================================================
-#  Dynamic Programming (top-down)                     =
-# =====================================================
-class Solution(object):
-    def __init__( self ) :
-        self.dp_table = {0: 0, 1: 1, 2: 1}
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
 
-    def fib(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n in self.dp_table: return self.dp_table[n]
-
-        val1, val2 = self.fib(n-1), self.fib(n-2)
-        if n-1 not in self.dp_table : self.dp_table[n-1] = val1
-        if n-2 not in self.dp_table : self.dp_table[n-2] = val2
-
-        return val1 + val2
-
-# =====================================================
-#  Dynamic Programming (bottom-up)                    =
-# =====================================================
-class Solution(object):
-    def __init__(self) :
-        self.dp_table = {0: 0, 1: 1, 2: 1}
-
-    def fib(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n in self.dp_table : return self.dp_table[n]
-        else :
-            for i in range( 3, n+1 ) :
-                self.dp_table[i] = self.dp_table[i-1] + self.dp_table[i-2]
-            return self.dp_table[n]
+    public int fib(int n) {
+        if( n == 0 || n == 1 ) return n;
+        
+        int first = 0, second = 1;
+        
+        for( int i=0 ; i<n-1 ; i++ ){
+            int tmp = first;
+            first = second;
+            second += tmp;
+        }
+        
+        return second;
+    }
+}
+==================================================================================================
 '''
