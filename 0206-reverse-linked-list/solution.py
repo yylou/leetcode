4c5dc4c6fe5434789682ebf51964184bbbfe9e12
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-12 16:10:14 +0800

    Update solution.py

--- 0206-reverse-linked-list/solution.py
+++ 0206-reverse-linked-list/solution.py
@@ -1,32 +1,74 @@
-##  Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        #: (base case)
+        if not head: return head
+        if not head.next: return head
+        
+        # ==================================================
+        #  Linked List                       (Iterative)   =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        prev = None
+        
+        while head:
+            #: STORE next node for next iteration
+            tmp = head.next
+            
+            #: RE-ASSIGN next pointer to prev node
+            head.next = prev
+            
+            #: ASSIGN prev node to current node
+            prev = head
+            
+            #: MOVE current node to next node
+            head = tmp
+        
+        return prev
+        
+        '''
+        # ==================================================
+        #  Linked List                       (Recursive)   =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+        #                                             5-4-n
+        # 1-2-3-4(head)-5(node)  ➜  1-2-3-4=5  ➜  1-2-3-4-n
+        #
+        #     5(node)-4-n             5-4-3       5-4-3-n
+        # 1-2-3(head)-4-n        ➜  1-2-3=4    ➜  1-2-3-n
+        
+        node = self.reverseList(head.next)
+        
+        #: recursion will end at the last-two element since head.next == None
+        head.next.next = head
+        head.next = None
+        
+        return node
+        '''
+        
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
 
-##  iterative solution
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-
-        ##  (edge case) null node or single node
-        if head      == None : return head
-        if head.next == None : return head
-
-        preNode = None
-        curNode = head
-
-        while curNode is not None :
-            tmpNode = curNode.next
-
-            ##  SWAP / REVERSE link
-            curNode.next = preNode
-            preNode = curNode
-
-            ##  move forward the current node
-            curNode = tmpNode
-
-        return preNode
+    public ListNode reverseList(ListNode head) {
+        if( head == null || head.next == null ) return head;
+        
+        ListNode prev = null;
+        while( head != null ){
+            ListNode tmp = head.next;
+            head.next = prev;
+            prev = head;
+            head = tmp;
+        }
+        return prev;
+    }
+}
+==================================================================================================
+'''
