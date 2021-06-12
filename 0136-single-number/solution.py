ca9b51214d98e632a7a70bb39b05909048bd8682
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-12 16:30:35 +0800

    Update solution.py

--- 0136-single-number/solution.py
+++ 0136-single-number/solution.py
@@ -1,17 +1,56 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        #: (base case)
+        if len(nums) == 1: return nums[0]
+        
+        # ==================================================
+        #  Python Set                                      =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+        
+        record = set()
+        for num in nums:
+            if num in record: record.remove(num)
+            else: record.add(num)
+        return record.pop()
+    
+        '''
+        # ==================================================
+        #  Bit Manipulation                                =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        # 0^a = a so the initial value of ret can be 0
+        ret = 0
+        for num in nums: ret ^= num
+        return ret
+        
+        # ==================================================
+        #  Math                                            =
+        # ==================================================
+        # time  : O(n+n+n) for set(nums) and sum operations
+        # space : O(n)     for set(nums)
+        
+        return 2 * sum(set(nums)) - sum(nums)
+        '''
+        
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
 
-        ##  Soluition (1) mathemetical solution: 2 * ( a+b+c ) - (a+a+b+b+c) = c
-        ##  use set to remove duplicate elements and sum the set, then minus the sum of original list
-        return 2 * sum( set( nums ) ) - sum( nums )
-
-        # =============================================================================================== #
-
-        ##  Solution (2) bit manipulation: 'a' xor 'b' xor 'a' = ( 'a' xor 'a' ) xor 'b' = 0 xor b = b
-        unique_num = 0
-        for num in nums : unique_num ^= num
-        return unique_num
+    public int singleNumber(int[] nums) {
+        int ret = 0;
+        for(int num : nums){
+            ret ^= num;
+        }
+        return ret;
+    }
+==================================================================================================
+'''
