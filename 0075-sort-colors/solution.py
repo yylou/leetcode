77361c831bcd2cffedac6fc2422ac67b6c37f579
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-17 17:46:07 +0800

    Update solution.py

--- 0075-sort-colors/solution.py
+++ 0075-sort-colors/solution.py
@@ -1,40 +1,89 @@
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
+        #: (base case)
+        if len(nums) == 1: return
+        
+        # ==================================================
+        #  Array + Three Pointers                          =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        p0, p2 = 0, len(nums)-1
+        
+        #: move pointer for '0' until meet non-0 element, check whether all numbers are '0'
+        while p0 < len(nums) and nums[p0] == 0: p0 += 1
+        if p0 == len(nums): return
+        
+        #: move pointer for '2' until meet non-2 element, check whether all numbers are '2'
+        while p2 >= 0 and nums[p2] == 2: p2 -= 1
+        if p2 == 0: return
+        
+        moveP = p0
+        
+        while moveP <= p2:
+            #: (ZERO)
+            if nums[moveP] == 0:
+                nums[moveP], nums[p0] = nums[p0], nums[moveP]
+                
+                p0 += 1
+                moveP += 1
+               
+            #: (TWO)
+            elif nums[moveP] == 2:
+                nums[moveP], nums[p2] = nums[p2], nums[moveP]
+                
+                p2 -= 1
+                
+            #: (ONE)
+            else: 
+                moveP += 1
+                
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
 
-        #:  (edge case) length == 1
-        n = len(nums)
-        if n == 1: return
-
-        #:  NOTE: index to PLACE '0' or '2'
-        zeroP, twoP = 0, n-1
-
-        #:  move pointer for '2' until meet non-2 element, check whether all numbers are '2'
-        while twoP >= 0 and nums[twoP] == 2: twoP -= 1
-        if twoP == 0: return
-
-        #:  move pointer for '0' until meet non-0 element, check whether all numbers are '0'
-        while zeroP < n and nums[zeroP] == 0: zeroP += 1
-        if zeroP == n: return
-
-        #:  'nums' does not have '1'
-        if twoP == zeroP: return
-
-        curP = zeroP
-
-        while curP <= twoP:
-            if nums[curP] == 0:
-                nums[curP], nums[zeroP] = nums[zeroP], nums[curP]
-                zeroP += 1
-                curP += 1
-
-            #:  meet '2', cur pointer stays in order to check whether swapped elemnt is '0'
-            elif nums[curP] == 2:
-                nums[curP], nums[twoP] = nums[twoP], nums[curP]
-                twoP -= 1
-
-            else:
-                curP += 1
+    public void sortColors(int[] nums) {
+        /* base case */
+        if(nums.length == 1) return;
+        
+        int p0 = 0, p2 = nums.length - 1;
+        
+        while(p0 < p2 && nums[p0] == 0) p0++;
+        if(p0 == p2) return;
+            
+        while(p2 <= 0 && nums[p2] == 2) p2--;
+        if(p2 == 0) return;
+            
+        int moveP = p0;
+        
+        while(moveP <= p2) {
+            if(nums[moveP] == 0) {
+                int tmp = nums[p0];
+                nums[p0] = nums[moveP];
+                nums[moveP] = tmp;
+                
+                moveP++;
+                p0++;
+            } else if(nums[moveP] == 2) {
+                int tmp = nums[p2];
+                nums[p2] = nums[moveP];
+                nums[moveP] = tmp;
+                
+                p2--;
+            } else {
+                moveP++;
+            }
+        }
+    }
+}
+==================================================================================================
+'''
