720f1fd0c083e805e54e0d3ca8369ef7513d9d75
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-17 18:50:29 +0800

    Update solution.py

--- 0104-maximum-depth-of-binary-tree/solution.py
+++ 0104-maximum-depth-of-binary-tree/solution.py
@@ -42,3 +42,77 @@ class Solution(object):
         ##  (Solution 2) Recursively visit in DFS manner and compare the MAX depth (one-liners)
         return max( self.maxDepth( root.left ) + 1, self.maxDepth( root.right ) + 1 )
     
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        #: (base case)
+        if not root: return 0
+        if not root.left and not root.right: return 1
+        
+        # ==================================================
+        #  Binary Tree                       (Recursive)   =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n), O(log(n)) for average case
+        
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+    
+        '''
+        # ==================================================
+        #  Binary Tree                       (Iterative)   =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n), O(log(n)) for average case
+
+        maxDepth = 0
+        stack = [root]
+        
+        #: (level-order iterative solution)
+        while stack:
+            #: loop through stack for current length, pop the first node
+            for i in range(len(stack)):
+                node = stack.pop( 0 )
+                
+                if node.left: stack.append(node.left)
+                if node.right: stack.append(node.right)
+                    
+            maxDepth += 1
+            
+        return maxDepth
+        '''
+        
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(n), O(log(n)) for average case
+     */
+
+    public int maxDepth(TreeNode root) {
+        /* base case */
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        
+        int maxDepth = 0;
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        stack.add(root);
+        
+        while(!stack.isEmpty()) {
+            int size = stack.size();
+            
+            for(int i=0 ; i<size ; i++) {
+                TreeNode node = stack.pollFirst();
+                
+                if(node.left != null) stack.add(node.left);
+                if(node.right != null) stack.add(node.right);
+            }
+            
+            maxDepth++;
+        }
+        
+        return maxDepth;
+    }
+}
+==================================================================================================
+'''
