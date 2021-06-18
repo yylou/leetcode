4db922ec3484ceed3eac3161d579c42b3b5b0587
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-18 13:50:33 +0800

    Update solution.py

--- 0108-convert-sorted-array-to-binary-search-tree/solution.py
+++ 0108-convert-sorted-array-to-binary-search-tree/solution.py
@@ -1,30 +1,59 @@
-#:  Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
-class Solution(object):
-    def sortedArrayToBST(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: TreeNode
-        """
-
-        #:  (edge case)
-        if len(nums) == 1: return TreeNode( nums[0] )
-
-        def subtree( left, right ):
-            if left > right: return None
-            if left == right: return TreeNode( nums[left] )
-
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        #: (base case)
+        if len(nums) == 1: return TreeNode(nums[0])
+        if len(nums) == 2: return TreeNode(nums[0], None, TreeNode(nums[1]))
+        
+        # ==================================================
+        #  Tree + DFS + Recursion                          =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+        
+        def subTree(left, right) -> TreeNode:
+            if left  > right: return None
+            if left == right: return TreeNode(nums[left])
+            
             center = (left + right) // 2
-
-            node = TreeNode( nums[center] )
-            node.right = subtree( center+1, right )
-            node.left = subtree( left, center-1 )
-
+            
+            node       = TreeNode(nums[center])
+            node.left  = subTree(left, center - 1)
+            node.right = subTree(center + 1, right)
+            
             return node
+            
+        return subTree(0, len(nums)-1)
+    
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(n)
+     */
 
-        return subtree( 0, len(nums)-1 )
+    public TreeNode subTree(int left, int right) {
+        if(left  > right) return null;
+        if(left == right) return new TreeNode(nums[left]);
+            
+        int center = (left + right) / 2;
+        
+        TreeNode node = new TreeNode(nums[center]);
+        node.left  = subTree(left, center - 1);
+        node.right = subTree(center + 1, right);
+        
+        return node;
+    }
+    
+    public TreeNode sortedArrayToBST(int[] nums) {
+        /* base case */
+        if(nums.length == 1) return new TreeNode(nums[0]);
+        
+        this.nums = nums;
+        
+        return subTree(0, nums.length - 1);
+    }
+}
+==================================================================================================
+'''
