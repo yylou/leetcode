2bac5b6fa865ee00fb5465c9c077ca812add82ea
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-19 12:34:27 +0800

    Update solution.py

--- 0118-pascals-triangle/solution.py
+++ 0118-pascals-triangle/solution.py
@@ -1,27 +1,64 @@
-class Solution(object):
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        #: (base case)
+        if numRows == 1: return [[1]]
+        if numRows == 2: return [[1], [1, 1]]
+        
+        # ==================================================
+        #  Array                                           =
+        # ==================================================
+        # time  : O(n^2)
+        # space : O(n^2)
+        
+        ret = [[1], [1, 1]]
+        
+        for i in range(numRows - 2):
+            tmp = [1]
+            
+            for j in range(0, len(ret[-1]) - 1):
+                tmp += [ret[-1][j] + ret[-1][j+1]]
+            
+            tmp += [1]
+            
+            ret.append(tmp)
+            
+        return ret
+    
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
 
-        ##  (edge case)
-        if numRows == 0 : return []
-        if numRows == 1 : return [[1]]
-        if numRows == 2 : return [[1], [1, 1]]
-
-        ret_value = [[1], [1, 1]]
-
-        ##  iteration starts from the second element of ret_value, and then loop for 'numRows - 2' times
-        for i in range( 1, numRows-1 ) :
-            tmp_list = [1]
-
-            ##  do the calculation of pairs of integers, then append into the temp list
-            for j in range( 1, len(ret_value[i]) ) :
-                tmp_list.append( ret_value[i][j] + ret_value[i][j-1] )
-
-            tmp_list.append( 1 )
-
-            ret_value.append( tmp_list )
-
-        return ret_value
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        
+        ret.add(new ArrayList<>());
+        ret.get(0).add(1);
+        
+        /* base case */
+        if(numRows == 1) return ret;
+        
+        for(int i=0 ; i<numRows-1 ; i++) {
+            List<Integer> tmp  = new ArrayList<>();
+            List<Integer> prev = ret.get(i);
+            
+            tmp.add(1);
+            
+            for(int j=0 ; j<prev.size()-1 ; j++) {
+                tmp.add(prev.get(j) + prev.get(j+1));
+            }
+            
+            tmp.add(1);
+            
+            ret.add(tmp);
+        }
+        
+        return ret;
+    }
+}
+==================================================================================================
+'''
