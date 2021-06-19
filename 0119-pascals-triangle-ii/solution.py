e424f6beb832f78b95fe64b61e5985eb2d580c33
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-19 13:39:27 +0800

    Update solution.py

--- 0119-pascals-triangle-ii/solution.py
+++ 0119-pascals-triangle-ii/solution.py
@@ -1,25 +1,54 @@
-##  slight tweak from the problem '118. Pascal's Triangle'
-class Solution(object):
-    def getRow(self, rowIndex):
-        """
-        :type rowIndex: int
-        :rtype: List[int]
-        """
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        #: (base case)
+        if rowIndex == 0: return [1]
+        if rowIndex == 1: return [1, 1]
+        
+        # ==================================================
+        #  Array + Math                                    =
+        # ==================================================
+        # time  : O(n^2)
+        # space : O(1)
+        
+        ret = [1, 1]
+        
+        #: (reverse-tracking for n-1 times)
+        #: calculate BACKWARDS to replace values without affecting the other elements
+        for i in range(rowIndex-1):
+            for j in range(len(ret)-1, 0, -1):
+                ret[j] = ret[j] + ret[j-1]
+            ret += [1]
+        
+        return ret
+            
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n^2)
+     * @space : O(1)
+     */
 
-        ##  edge case handling
-        if rowIndex == 0 : return [1]
-        if rowIndex == 1 : return [1, 1]
-        if rowIndex == 2 : return [1, 2, 1]
-
-        ret_value = [1, 2, 1]
-
-        ##  'numRows - 2' times of iteration (since current ret_value is at rowIndex 2)
-        for i in range( 0, rowIndex-2 ):
-
-            ##  do the calculation BACKWARDS in order to replace the value without affecting the other calculation
-            for j in range( len(ret_value)-1, 0, -1 ):
-                ret_value[j] = ret_value[j] + ret_value[j-1]
-
-            ret_value.append( 1 )
-
-        return ret_value
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> ret = new ArrayList<Integer>();
+        
+        ret.add(1);
+        
+        /* base case */
+        if(rowIndex == 0) return ret;
+        
+        ret.add(1);
+        
+        for(int i=0 ; i<rowIndex-1 ; i++) {
+            for(int j=ret.size()-1 ; j>0 ; j--) {
+                ret.set(j, ret.get(j) + ret.get(j-1));
+            }
+            ret.add(1);            
+        }
+        
+        return ret;
+    }
+}
+==================================================================================================
+'''
