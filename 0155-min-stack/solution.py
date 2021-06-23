25606e625a5a42b3a9f4af0b4cdd8ec7d3ba172d
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-23 15:17:06 +0800

    Update solution.py

--- 0155-min-stack/solution.py
+++ 0155-min-stack/solution.py
@@ -1,45 +1,59 @@
-class MinStack(object):
+class MinStack:
+    # time  : O(1) for all operations
+    # space : O(n)
 
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
         self.stack = []
 
+    def push(self, val: int) -> None:
+        if not self.stack: self.stack.append((val, val))
+        else: self.stack.append((val, min(val, self.stack[-1][1])))
 
-    def push(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
-        if not self.stack: self.stack.append( (val, val) )
-        else: self.stack.append( (val, min( val, self.stack[-1][1]) ) )
-
-
-    def pop(self):
-        """
-        :rtype: None
-        """
+    def pop(self) -> None:
         self.stack.pop()
 
-
-    def top(self):
-        """
-        :rtype: int
-        """
+    def top(self) -> int:
         return self.stack[-1][0]
 
-
-    def getMin(self):
-        """
-        :rtype: int
-        """
+    def getMin(self) -> int:
         return self.stack[-1][1]
 
 
-##  Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+'''
+Java Solution
+==================================================================================================
+class MinStack {
+    /**
+     * @time  : O(1) for all operations
+     * @space : O(n)
+     */
+
+    private Stack<int[]> stack;
+    
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(stack.isEmpty()) {
+            stack.push(new int[]{val, val});
+        } else {
+            int curMin = stack.peek()[1];
+            stack.push(new int[]{val, Math.min(val, curMin)});
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek()[0];
+    }
+    
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+==================================================================================================
+'''
