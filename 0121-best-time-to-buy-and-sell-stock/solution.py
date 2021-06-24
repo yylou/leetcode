12fb42127ea2afac4ae642d55ec72d74c1152a3c
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-24 22:51:12 +0800

    Update solution.py

--- 0121-best-time-to-buy-and-sell-stock/solution.py
+++ 0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,52 +1,63 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-
-        #:  (edge case) n == 1
-        n = len(prices)
-        if n == 1: return 0
-
-
-        #:  Solution (1) reduced DP: iterate while maintaining local-min
-        localmin, maxProfit = float('inf'), -float('inf')
-        for i in xrange( n ):
-            if prices[i] < localmin: localmin = prices[i]
-            tmpResult = prices[i] - localmin
-            if tmpResult > maxProfit: maxProfit = tmpResult
-
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #  (base case)
+        if len(prices) == 1: return 0
+        
+        # ==================================================
+        #  Array + Dynamic Programming         (localMin)  =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        localMin, maxProfit = float('inf'), float('-inf')
+        for i in range(len(prices)):
+            if prices[i] < localMin: localMin = prices[i]
+                
+            tmpProfit = prices[i] - localMin
+            if tmpProfit > maxProfit: maxProfit = tmpProfit
+                
         return maxProfit
-
-
-        # =========================================================================== #
-
-
-        #:  Solution (2) standard DP: record local-min and local-max to calculate profit
-        #:  - time complexity: O(n)
-        #:  - space complexity: O(n)
-
-        #:  (a) local min from left
-        minRecord = [''] * n
-        for i in xrange( n ):
-            if i == 0: minRecord[i] = prices[i]
-            else:
-                if prices[i] < minRecord[i-1]: minRecord[i] = prices[i]
-                else: minRecord[i] = minRecord[i-1]
-
-        #:  (b) local max from right
-        maxRecord = [''] * n
-        for i in xrange( n-1, -1, -1 ):
-            if i == n-1: maxRecord[i] = prices[i]
-            else:
-                if prices[i] > maxRecord[i+1]: maxRecord[i] = prices[i]
-                else: maxRecord[i] = maxRecord[i+1]
-
-        #:  (c) find the max profit
-        maxProfit = -float('inf')
-        for i in xrange( n ):
-            tmpResult = maxRecord[i] - minRecord[i]
-            if tmpResult > maxProfit: maxProfit = tmpResult
-
+        
+        '''
+        # ==================================================
+        #  Array + Dynamic Programming         (localMax)  =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        localMax, maxProfit = float('-inf'), float('-inf')
+        for i in range(len(prices)-1, -1, -1):
+            if prices[i] > localMax: localMax = prices[i]
+            
+            tmpProfit = localMax - prices[i]
+            if tmpProfit > maxProfit: maxProfit = tmpProfit
+                
         return maxProfit
+        '''
+        
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
+     
+    public int maxProfit(int[] prices) {
+        /* base case */
+        if(prices.length == 1) return 0;
+        
+        int localMin = Integer.MAX_VALUE, maxProfit = Integer.MIN_VALUE;
+        for(int i=0 ; i<prices.length ; i++) {
+            if(prices[i] < localMin) localMin = prices[i];
+                
+            int tmpProfit = prices[i] - localMin;
+            if(tmpProfit > maxProfit) maxProfit = tmpProfit;
+        }
+        
+        return maxProfit;
+    }
+}
+==================================================================================================
+'''
