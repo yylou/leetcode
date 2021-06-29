a4a050c3da79dd155b71cfb1d851cc876b8780b5
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-29 16:49:26 +0800

    Update solution.py

--- 0217-contains-duplicate/solution.py
+++ 0217-contains-duplicate/solution.py
@@ -1,22 +1,45 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        n = len(nums)
-        if n == 1: return False
-
-        #:  Solution (1) one-liner by using SET
-        return True if len( set(nums) ) < n else False
-
-
-        # ======================================================== #
-
-
-        #:  Solution (2) iterative
-        record = set()
-        for element in nums:
-            if element in record: return True
-            record.add( element )
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        #  (base case)
+        if len(nums) == 1: return False
+        
+        # ==================================================
+        #  Array + Hash Table                              =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+        
+        table = set()
+        
+        for num in nums:
+            if num not in table: table.add(num)
+            else: return True
+        
         return False
+    
+        '''
+        return len(nums) != len(set(nums))
+        '''
+        
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(n)
+     */
+    
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>(nums.length);
+        
+        for (int x: nums) {
+            if (set.contains(x)) return true;
+            set.add(x);
+        }
+        
+        return false;
+    }
+}
+==================================================================================================
+'''
