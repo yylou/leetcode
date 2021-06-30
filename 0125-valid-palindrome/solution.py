40630ff11da5d158d783e8a8468f4c11b7bcdc88
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-30 16:50:39 +0800

    Update solution.py

--- 0125-valid-palindrome/solution.py
+++ 0125-valid-palindrome/solution.py
@@ -1,26 +1,55 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-
-        n = len(s)
-
-        ##  (edge case) s only has one char
-        if n == 1: return True
-
-        s = s.lower()
-
-        ##  Solution (1) two-pointer
-        left, right = 0, n-1
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #  (base case)
+        if len(s) == 1: return True
+        
+        # ==================================================
+        #  String + Two Pointer                            =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        left, right = 0, len(s) - 1
+         
         while left < right:
-            while left < right and not s[left].isalnum(): left += 1
-            while left < right and not s[right].isalnum(): right -= 1
-
-            if s[left] != s[right]: return False
-
-            left += 1
-            right -=1
-
+            while left  < len(s) and not s[left].isalnum(): left += 1
+            while right >= 0     and not s[right].isalnum(): right -= 1
+                
+            if left > right or s[left].lower() != s[right].lower(): return True
+            
+            left  += 1
+            right -= 1
+            
         return True
+    
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
+     
+    public boolean isPalindrome(String s) {
+        /* base case */
+        if(s.length() == 1) return true;
+        
+        int left = 0, right = s.length() - 1;
+        
+        while(left < right) {
+            while(left < s.length() && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while(right >= 0 && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            
+            if(left > right) return true;
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+            
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
+==================================================================================================
+'''
