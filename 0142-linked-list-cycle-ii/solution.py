8730401f701464ffa9a34d65648bac18b2742a90
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-30 15:31:10 +0800

    Update solution.py

--- 0142-linked-list-cycle-ii/solution.py
+++ 0142-linked-list-cycle-ii/solution.py
@@ -1,57 +1,64 @@
-##  Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution(object):
-    def detectCycle(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-
-        ##  (edge case) root is null / root has no next node
-        if not head: return None
-        if not head.next: return None
-
-
-        ##  Solution (1) Floyd's algorithm
-        ##  - time complexity: O(n)
-        ##  - space complexity: O(1)
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        #  (base case)
+        if not head or not head.next: return None
+        if head == head.next.next: return head
+        
+        # ==================================================
+        #  Linked List + Two Pointer  (Floyd's algorithm)  =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
         slowP, fastP = head, head
-
-        ##  (1) find the intersection
-        while True:
-            ##  reach the end, RETURN
-            if slowP == None: return None
-            if fastP == None or fastP.next == None: return None
-
+        
+        #  (find the intersection)
+        while fastP and fastP.next:
             slowP = slowP.next
             fastP = fastP.next.next
-
             if slowP == fastP: break
-
-        ##  (2) find the entrance of the cycle
+                
+        if slowP != fastP: return None
+        
+        #  (find the cycle entrance)
         slowP = head
         while slowP != fastP:
             slowP = slowP.next
             fastP = fastP.next
-
+        
         return slowP
-
-
-        # ============================================================ #
-
-
-        ##  Solution (2) using set to record the existing node
-        ##  - time complexity: O(n)
-        ##  - space complexity: O(n)
-        record = set()
-        while head is not None:
-            if head in record: return head
-            record.add( head )
-
-            head = head.next
-
-        return None
+    
+'''
+Java Solution
+==================================================================================================
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(1)
+     */
+    
+    public ListNode detectCycle(ListNode head) {
+        /* case case */
+        if(head == null || head.next == null) return null;
+        if(head == head.next.next) return head;
+        
+        ListNode slowP = head, fastP = head;
+        while(fastP != null && fastP.next != null) {
+            slowP = slowP.next;
+            fastP = fastP.next.next;
+            if(slowP == fastP) break;
+        }
+        
+        if(slowP != fastP) return null;
+        
+        slowP = head;
+        while(slowP != fastP) {
+            slowP = slowP.next;
+            fastP = fastP.next;
+        }
+        
+        return slowP;
+    }
+}
+==================================================================================================
+'''
