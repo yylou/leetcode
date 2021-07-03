c8fdd2b69dd53e7055e58bfd5ad03c3f816ffe35
Yuan-Yao Lou <yylou0701@gmail.com>
2021-07-03 19:09:00 +0800

    Update solution.py

--- 0208-implement-trie-prefix-tree/solution.py
+++ 0208-implement-trie-prefix-tree/solution.py
@@ -1,33 +1,23 @@
 class Trie:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.root = {}
         
 
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
         # time  : O(n), n is the lenght of str
         # space : O(n)
         
         cur = self.root
         
         for char in word:
-            if char not in cur: cur[char] = {}
-            
+            if char not in cur: cur[char] = {}            
             cur = cur[char]
             
         cur['#'] = True
         
 
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
         # time  : O(n)
         # space : O(1)
         
@@ -35,16 +25,12 @@ class Trie:
         
         for char in word:
             if char not in cur: return False
-            
             cur = cur[char]
             
         return '#' in cur
         
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
         # time  : O(n)
         # space : O(1)
         
@@ -52,7 +38,6 @@ class Trie:
         
         for char in prefix:
             if char not in cur: return False
-            
             cur = cur[char]
             
         return True
