854366a4134a798b2907cfc1c6a42e7d0e270836
Yuan-Yao Lou <yylou0701@gmail.com>
2021-07-07 23:14:34 +0800

    Create solution.java

---
+++ 0001-2-sum/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    /**  
+     * @time  : O(n)
+     * @space : O(n)
+     */
+    
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            int remain = target - nums[i];
+            if(map.containsKey(remain)) return new int[] {i, map.get(remain)};
+            
+            map.put(nums[i], i);
+        }
+        
+        return null;
+    }
+}
