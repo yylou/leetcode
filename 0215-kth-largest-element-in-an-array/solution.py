a92178d71bab4fe4164a5b74a6c098fb82bd2adf
Yuan-Yao Lou <yylou0701@gmail.com>
2021-07-09 14:42:51 +0800

    Update solution.py

--- 0215-kth-largest-element-in-an-array/solution.py
+++ 0215-kth-largest-element-in-an-array/solution.py
@@ -1,100 +1,38 @@
-class Solution(object):
-    def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-
-        ##  (edge case) k == 1
-        if k == 1: return max(nums)
-
-
-        #:  Solution (1) "Hoare's selection algorithm"
-        #:  - time complexity: O(n)
-        #:  - space complexity: O(1)
-        def partition( left, right, index ):
-            pivot = nums[index]
-
-            #:  move pivot to the last (right)
-            nums[index], nums[right] = nums[right], nums[index]
-
-            #:  move all smaller elements to the left
-            placeIndex = left
-            for i in xrange( left, right ):
-                if pivot > nums[i]:
-                    nums[placeIndex], nums[i] = nums[i], nums[placeIndex]
-                    placeIndex += 1
-
-            #:  move pivot back to split point
-            nums[placeIndex], nums[right] = nums[right], nums[placeIndex]
-
-            return placeIndex
-
-
-        def quickSelect( left, right, k_smallest ):
-            if left == right: return nums[left]
-
-            #:  assign pivot by the mid point and do partition
-            index = partition( left, right, left + (right-left)/2 )
-
-            if index == k_smallest: return nums[index]
-            elif index > k_smallest: return quickSelect( left, index-1, k_smallest )
-            else: return quickSelect( index+1, right, k_smallest )
-
-
-        n = len(nums)
-        return quickSelect( 0, n-1, n-k )  #:  convert to k-smallest problem
-
-
-        # ======================================================================== #
-
-
-        #:  Solution (2) sort
-        #:  - time complexity: O(nlogn)
-        #:  - space complexity: O(1)
-        def mergeSort( array ):
-            if len(array) > 1:
-                mid = len(array) // 2
-                arrayL, arrayR = array[:mid], array[mid:]
-
-                mergeSort(arrayL)
-                mergeSort(arrayR)
-
-                lenL, lenR = len(arrayL), len(arrayR)
-
-                pL = pR = curP = 0
-
-                #:  merge two sorted lists
-                while pL < lenL and pR < lenR:
-                    if arrayL[pL] < arrayR[pR]:
-                        array[curP] = arrayL[pL]
-                        pL += 1
-                    else:
-                        array[curP] = arrayR[pR]
-                        pR += 1
-                    curP += 1
-
-                #:  checking remaining elements
-                while pL < lenL:
-                    array[curP] = arrayL[pL]
-                    pL += 1
-                    curP += 1
-
-                while pR < lenR:
-                    array[curP] = arrayR[pR]
-                    pR += 1
-                    curP += 1
-
-        # mergeSort( nums )
-        nums.sort()
-        return nums[len(nums)-k]
-
-
-        # ======================================================================== #
-
-
-        #:  Solution (3) using python built-in priority-queue (heap)
-        #:  - time complexity: O(nlogk)
-        #:  - space complexity: O(k)
-        return heapq.nlargest(k, nums)[-1]
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # (base case)
+        if len(nums) == 1: return nums[0]
+        
+        # ==================================================
+        #  Array + Quickselect (k largest = n-k smallest)  =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
+        
+        self.nums = nums
+        return self.quickSelect(0, len(nums) - 1, k - 1)
+    
+    def quickSelect(self, left, right, kLargest) -> int:
+        while left <= right:
+            pivot = self.partition(left, right)
+            if pivot == kLargest: return self.nums[kLargest]
+            elif pivot > kLargest: right = pivot - 1
+            else: left = pivot + 1
+                
+    def partition(self, left, right) -> int:
+        randomNum = (left + right) // 2
+        
+        #  move pivot to the end/right
+        pivot = right
+        self.nums[randomNum], self.nums[pivot] = self.nums[pivot], self.nums[randomNum]
+        
+        #  move all larger elements to the left
+        placeP = left
+        for i in range(left, right):
+            if self.nums[i] >= self.nums[pivot]:
+                self.nums[placeP], self.nums[i] = self.nums[i], self.nums[placeP]
+                placeP += 1
+            
+        #  move back the pivot to make an almost-sorted array
+        self.nums[placeP], self.nums[pivot] = self.nums[pivot], self.nums[placeP]
+        return placeP
