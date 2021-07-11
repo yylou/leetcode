9dc4f5230386e48c3a41622322f42928592ab7c6
Yuan-Yao Lou <yylou0701@gmail.com>
2021-07-11 19:13:16 +0800

    Update solution.py

--- 0021-merge-two-sorted-lists/solution.py
+++ 0021-merge-two-sorted-lists/solution.py
@@ -1,10 +1,23 @@
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         #  (base case)
-        if not l1 and l2: return l2
         if not l2 and l1: return l1
+        if not l1 and l2: return l2
         if not l1 and not l2: return None
         
+        # ==================================================
+        #  Linked List + Recursion                         =
+        # ==================================================
+        # n is the length of l1, and m is the length of l2
+        # time  : O(n+m)
+        # space : O(n+m)
+            
+        if l1.val > l2.val: l1, l2 = l2, l1
+        l1.next = self.mergeTwoLists(l1.next, l2)
+        
+        return l1
+        
+        '''
         # ==================================================
         #  Linked List + Two Pointers                      =
         # ==================================================
@@ -31,51 +44,4 @@ class Solution:
         cur.next = l1 or l2
             
         return ret.next
-    
         '''
-        # ==================================================
-        #  Linked List + Recursion                         =
-        # ==================================================
-        # n is the length of l1, and m is the length of l2
-        # time  : O(n+m)
-        # space : O(n+m)
-            
-        if l1 and l2:
-            if l1.val > l2.val: l1, l2 = l2, l1
-            l1.next = self.mergeTwoLists(l1.next, l2)
-        return l1 or l2
-        '''
-    
-'''
-Java Solution
-==================================================================================================
-class Solution {
-    /**
-     * @time  : O(n+m)
-     * @space : O(1)
-     */
-
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode ret = new ListNode(0);
-
-        ListNode cur = ret;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
-                
-            } else {
-                cur.next = l2;
-                l2 = l2.next;
-            }
-            
-            cur = cur.next;
-        }
-
-        cur.next = l1 == null ? l2 : l1;
-
-        return ret.next;
-    }
-}
-==================================================================================================
-'''
