45d58c813125be37982301d61ccfa55cc36ec232
Yuan-Yao Lou <yylou0701@gmail.com>
2021-07-12 20:22:17 +0800

    Update solution.py

--- 0003-longest-substring-without-repeating-characters/solution.py
+++ 0003-longest-substring-without-repeating-characters/solution.py
@@ -1,12 +1,7 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        ans, slowP, fastP = 0, 0, 0
-        hashTable = dict()
-        
-        length = len( s )
-        
-        #:  (edge case)
-        if length == 0: return 0
+        # (base case)
+        if not s: return 0
         
         # ==================================================
         #  Hash Table + Sliding Window (Two Pointer)       =
@@ -14,54 +9,18 @@ class Solution:
         # time  : O(n)
         # space : O(n)
         
-        while slowP < length and fastP < length:
-            #:  (1) new char: record char in hash table and extend sliding window by fast pointer
-            if s[fastP] not in hashTable:
-                hashTable[s[fastP]] = fastP
+        table = set()
+        slowP, fastP = 0, 0
+        ans = float('-inf')
+        
+        while fastP < len(s):
+            if s[fastP] not in table:
+                table.add(s[fastP])
                 fastP += 1
-                ans = max( ans, fastP-slowP )
+                ans = max(ans, fastP - slowP)
                 
-            #:  (2) repeating char: delete record in hash table and shrink sliding window by slow pointer
             else:
-                del hashTable[s[slowP]]
+                table.remove(s[slowP])
                 slowP += 1
                 
         return ans
-
-    
-
-'''
-Java Solution
-==================================================================================================
-class Solution {
-    /**  
-     * @time  : O(n)
-     * @space : O(n)
-     */
-    public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> hashTable = new HashMap<>();
-        int ans = 0, slowP = 0, fastP = 0;
-        
-        int length = s.length();
-        if( length == 0 ){ return 0; }
-        
-        while( fastP < length ){
-            char curChar = s.charAt( fastP );
-            
-            if( !hashTable.containsKey( curChar ) ){
-                hashTable.put( curChar, fastP );
-                fastP++;
-                ans = Math.max( ans, fastP-slowP );
-                
-            } else {
-                curChar = s.charAt( slowP );
-                hashTable.remove( curChar );
-                slowP++;
-            }
-        }
-        
-        return ans;
-    }
-}
-==================================================================================================
-'''
