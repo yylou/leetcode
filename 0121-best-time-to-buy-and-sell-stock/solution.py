0398babc919e30a7176bca3a514ee9d38d18cc74
Yuan-Yao Lou <yylou0701@gmail.com>
2021-07-13 18:38:07 +0800

    Update solution.py

--- 0121-best-time-to-buy-and-sell-stock/solution.py
+++ 0121-best-time-to-buy-and-sell-stock/solution.py
@@ -57,30 +57,3 @@ class Solution:
         #  (HOLD state does not have MAX profit)
         return noHold
         '''
-        
-'''
-Java Solution
-==================================================================================================
-class Solution {
-    /**
-     * @time  : O(n)
-     * @space : O(1)
-     */
-     
-    public int maxProfit(int[] prices) {
-        /* base case */
-        if(prices.length == 1) return 0;
-        
-        int localMin = Integer.MAX_VALUE, maxProfit = Integer.MIN_VALUE;
-        for(int price: prices) {
-            if(price < localMin) localMin = price;
-                
-            int tmpProfit = price - localMin;
-            if(tmpProfit > maxProfit) maxProfit = tmpProfit;
-        }
-        
-        return maxProfit;
-    }
-}
-==================================================================================================
-'''
