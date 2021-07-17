4c87d0def1af1f0a334d78b42456cce8b33adda0
Yuan-Yao Lou <yylou0701@gmail.com>
2021-07-17 22:09:50 +0800

    Update solution.py

--- 0023-merge-k-sorted-lists/solution.py
+++ 0023-merge-k-sorted-lists/solution.py
@@ -1,33 +1,40 @@
-##  Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-class Solution(object):
-    def mergeKLists(self, lists):
-        """
-        :type lists: List[ListNode]
-        :rtype: ListNode
-        """
-
-        ##  RECORD all the values in all linked lists
-        valList = []
-        for element in lists:
-            while element:
-                valList.append( element.val )
-                element = element.next
-
-        ##  SORT (time complexity: O(nlogn))
-        valList.sort()
-
-        ##  Loop through sorted list to build linked list
-        head = ListNode()
-        retNode = head
-
-        for val in valList:
-            node = ListNode( val )
-            head.next = node
-            head = node
-
-        return retNode.next
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        # (base case)
+        if len(lists) == 0: return None
+        if len(lists) == 1: return lists[0]
+        
+        # ==================================================
+        #  Linked List + Sort                              =
+        # ==================================================
+        # time  : O(nlogk)
+        # space : O(1)
+        # 
+        # n is the total number of nodes in two linked lists
+        # k is the number of linked lists
+        
+        size = 1
+        while size < len(lists):
+            for i in range(0, len(lists) - size, size * 2):
+                lists[i] = self.merge2Lists(lists[i], lists[i+size])
+            size *= 2
+            
+        return lists[0]
+        
+    def merge2Lists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        ret = ListNode(0)
+        cur = ret
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = ListNode(l1.val)
+                cur = cur.next
+                l1 = l1.next
+            else:
+                cur.next = ListNode(l2.val)
+                cur = cur.next
+                l2 = l2.next
+                
+        cur.next = l1 or l2
+                
+        return ret.next
