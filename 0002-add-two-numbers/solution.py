679554da73205ff834b4522b913195002a71573a
Yuan-Yao Lou <yylou0701@gmail.com>
2021-07-19 22:03:03 +0800

    Update solution.py

--- 0002-add-two-numbers/solution.py
+++ 0002-add-two-numbers/solution.py
@@ -1,9 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         cur   = ListNode()
@@ -31,50 +25,7 @@ class Solution:
             if l1: l1 = l1.next
             if l2: l2 = l2.next
         
-        #:  check carry to see whether creating extra node is needed
+        #  check carry to see whether creating extra node is needed
         if carry != 0: cur.next = ListNode( carry )
             
         return ret.next
-    
-    
-    
-'''
-Java Solution
-==================================================================================================
-class Solution {
-    /**  
-     * @time  : O(max(n, m))
-     * @space : O(1)
-     */
-    
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode cur = new ListNode();
-        ListNode ret = cur;
-        int carry = 0;
-        
-        while(l1 != null || l2 != null) {
-            int num1, num2;
-            
-            if(l1 != null) num1 = l1.val;
-            else num1 = 0;
-            
-            if(l2 != null) num2 = l2.val;
-            else num2 = 0;
-            
-            int sum = num1 + num2 + carry;
-            carry = sum / 10;
-            sum = sum % 10;
-            
-            cur.next = new ListNode(sum);
-            cur = cur.next;
-            
-            if(l1 != null) l1 = l1.next;
-            if(l2 != null) l2 = l2.next;
-        }
-        
-        if(carry != 0) cur.next = new ListNode(carry);
-        return ret.next;
-    }
-}
-==================================================================================================
-'''
