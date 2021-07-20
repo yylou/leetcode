d29163a1cd5f4d08d1eebbfc80e865dfcd7dbc5e
Yuan-Yao Lou <yylou0701@gmail.com>
2021-07-20 15:49:11 +0800

    Update solution.java

--- 0002-add-two-numbers/solution.java
+++ 0002-add-two-numbers/solution.java
@@ -12,11 +12,15 @@ class Solution {
         while(l1 != null || l2 != null) {
             int num1, num2;
             
-            if(l1 != null) num1 = l1.val;
-            else num1 = 0;
+            if(l1 != null) {
+                num1 = l1.val;
+                l1 = l1.next;
+            } else num1 = 0;
             
-            if(l2 != null) num2 = l2.val;
-            else num2 = 0;
+            if(l2 != null) {
+                num2 = l2.val;
+                l2 = l2.next;
+            } else num2 = 0;
             
             int sum = num1 + num2 + carry;
             carry = sum / 10;
@@ -24,9 +28,6 @@ class Solution {
             
             cur.next = new ListNode(sum);
             cur = cur.next;
-            
-            if(l1 != null) l1 = l1.next;
-            if(l2 != null) l2 = l2.next;
         }
         
         if(carry != 0) cur.next = new ListNode(carry);
