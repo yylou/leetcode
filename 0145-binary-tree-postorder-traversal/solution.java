ad896b0ec5fe92a08c7caf46ac15477f98610549
Yuan-Yao Lou <yyloumike@gmail.com>
2021-07-22 14:09:45 +0800

    Add Solution (Python + Java)

---
+++ 0145-binary-tree-postorder-traversal/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @time  : O(n)
+     * @space : O(n)
+     */
+    
+    public List<Integer> postorderTraversal(TreeNode root) {
+        /* base case */
+        if(root == null) return new ArrayList<>();
+        
+        List<Integer> result = new ArrayList<>();
+        Stack<Pair<TreeNode, Boolean>> stack = new Stack<Pair<TreeNode, Boolean>>();
+        stack.push(new Pair<TreeNode, Boolean>(root, false));
+        
+        while(!stack.isEmpty()) {
+            Pair<TreeNode, Boolean> object = stack.pop();
+            TreeNode node = object.getKey();
+            Boolean visited = object.getValue();
+            
+            if(visited == true) result.add(node.val);
+            else {
+                stack.push(new Pair<TreeNode, Boolean>(node, true));
+                if(node.right != null) stack.push(new Pair<TreeNode, Boolean>(node.right, false));
+                if(node.left != null) stack.push(new Pair<TreeNode, Boolean>(node.left, false));
+            }
+        }
+        
+        return result;
+    }
+}
--- 0145-binary-tree-postorder-traversal/solution.py
+++ 0145-binary-tree-postorder-traversal/solution.py
@@ -1,80 +1,37 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def postorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-
-        ## (edge case) root == none, root's children == none
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        # (base case)
         if not root: return []
         if not root.left and not root.right: return [root.val]
-
-        ##  (1) Iterative solution:
-        retVal = []
-        stack = [root]
-        prevNode = None
-
-        while stack:
-            ##  (debug)
-            # print map(lambda x: x.val, stack), retVal[::-1]
-
-            curNode = stack[-1]
-
-            if ( not curNode.left and not curNode.right ) or ( prevNode and ( prevNode == curNode.right or prevNode == curNode.left ) ):
-                retVal.append(curNode.val)
-                stack.pop()
-                prevNode = curNode
-
-            else:
-                if curNode.right: stack.append(curNode.right)
-                if curNode.left: stack.append(curNode.left)
-
-        return retVal
-
-        ##  ==================================================================================  ##
-
-        ##  (2) Iterative solution: REVERSE preorder traversal
-        retVal = []
-        stack = [root]
-
-        while stack:
-            ##  (debug)
-            # print map(lambda x: x.val, stack), retVal[::-1]
-
-            ##  APPEND the value at first, then traverse
-            curNode = stack.pop()
-            retVal.append(curNode.val)
-
-            ##  append RIGHT at first due to the order of Python list data-structure
-            if curNode.left: stack.append(curNode.left)
-            if curNode.right: stack.append(curNode.right)
-
-        return retVal[::-1]
-
-        ##  ==================================================================================  ##
-
-        ##  (3) recursive solution
-        retVal = []
-
-        def recursive(node, retVal):
-            ##  TRAVERSE at first, then append the value
-            if node.left: recursive(node.left, retVal)
-            if node.right: recursive(node.right, retVal)
-
-            retVal.append(node.val)
-
-        recursive(root, retVal)
-
-        return retVal
-
-        ##  ==================================================================================  ##
-
-        ##  (3) recursive solution
-        return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
         
+        # ==================================================
+        #  Binary Tree + Post-order Traversal              =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+        
+        ans = []
+        stack = [(root, False)]
+        
+        while stack:
+            node, visited = stack.pop()
+            
+            if node:
+                if visited:
+                    ans.append(node.val)
+                else:
+                    stack.append((node, True))
+                    stack.append((node.right, False))
+                    stack.append((node.left, False))
+                    
+        return ans
+    
+        '''
+        # ==================================================
+        #  Binary Tree + Post-order Traversal              =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+        
+        return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+        '''
