beb8fafa741abaaf4e3e7781d3574f4b06811527
Yuan-Yao Lou <yyloumike@gmail.com>
2021-07-22 12:45:16 +0800

    Remove Performance Figure

--- 0094-binary-tree-inorder-traversal/solution.py
+++ 0094-binary-tree-inorder-traversal/solution.py
@@ -1,57 +1,27 @@
-##  Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
-class Solution(object):
-    def inorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-
-        ##  (edge case) root is none, root has not children
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        # (base case)
         if not root: return []
         if not root.left and not root.right: return [root.val]
-
-        ##  (1) iterative solution
-        retVal = []
-        stack = []
-        curNode = root
-
-        while stack or curNode:
-            ##  keep visiting left node until achieve at the leaf
-            while curNode:
-                stack.append(curNode)
-                curNode = curNode.left
-
-            ##  pop out the last added node and append its value
-            curNode = stack.pop()
-            retVal.append(curNode.val)
-
-            ##  assign the right node to the current node
-            ##  --> checking end condition (if right node is none) or check the right branch
-            curNode = curNode.right
-
-        return retVal
-
-        ##  ==================================================================================  ##
-
-        ##  (2) recursive solution
-        retVal = []
-
-        def recursive(node, retVal):
-            if node.left: recursive(node.left, retVal)
-            retVal.append(node.val)
-            if node.right: recursive(node.right, retVal)
-
-        recursive(root, retVal)
-
-        return retVal
-
-        ##  ==================================================================================  ##
-
-        ##  (3) recursive solution
-        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+        
+        # ==================================================
+        #  Binary Tree + In-order Traversal               =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+        
+        ret = []
+        stack = [(root, False)]
+        
+        while stack:
+            node, visited = stack.pop()
+            
+            if visited:
+                ret.append(node.val)
+                
+            else:
+                if node.right: stack.append((node.right, False))
+                stack.append((node, True))
+                if node.left: stack.append((node.left, False))
+            
+        return ret
