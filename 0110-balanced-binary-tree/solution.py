ff22c0ec67ac64a8eb5e75916849e8c272baea3a
Yuan-Yao Lou <yyloumike@gmail.com>
2021-07-26 13:51:06 +0800

    Add Solution (Python + Java)

--- 0110-balanced-binary-tree/solution.py
+++ 0110-balanced-binary-tree/solution.py
@@ -1,17 +1,4 @@
 class Solution:
-    def dfs(self, node: TreeNode) -> bool:
-        if not node: return 0
-        if not node.left and not node.right: return 1
-        
-        leftH = self.dfs(node.left)
-        if leftH == -1: return -1
-        
-        rightH = self.dfs(node.right)
-        if rightH == -1: return -1
-        
-        if abs(leftH - rightH) > 1: return -1
-        return max(leftH, rightH) + 1
-    
     def isBalanced(self, root: TreeNode) -> bool:
         # (base case)
         if not root: return True
@@ -25,6 +12,19 @@ class Solution:
         
         return self.dfs(root) != -1
     
+    def dfs(self, node: TreeNode) -> bool:
+        if not node: return 0
+        if not node.left and not node.right: return 1
+        
+        leftH = self.dfs(node.left)
+        if leftH == -1: return -1
+        
+        rightH = self.dfs(node.right)
+        if rightH == -1: return -1
+        
+        if abs(leftH - rightH) > 1: return -1
+        return max(leftH, rightH) + 1
+        
     '''
     def isBalanced(self, root: TreeNode) -> bool:
         # (base case)
