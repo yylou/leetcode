75fadd50ffcb98139af87e4596da87043c2d9fc3
Yuan-Yao Lou <yyloumike@gmail.com>
2021-08-01 00:08:30 +0800

    Add Solution (Python)

--- 0170-two-sum-iii-data-structure-design/solution.py
+++ 0170-two-sum-iii-data-structure-design/solution.py
@@ -20,23 +20,23 @@ class TwoSum:
                 return True
             elif self.data[start] + self.data[end] > value:
                 # result may be on left side
-                end = bisect_right(self.data, value - self.data[start], start+1, end) - 1
+                end = bisect_right(self.data, value - self.data[start], start + 1, end) - 1
             else:
                 # result may be on right side
-                start = bisect_left(self.data, value - self.data[end], start+1, end - 1)
+                start = bisect_left(self.data, value - self.data[end], start + 1, end - 1)
             
         return False
 
     def bisect_right(target: int, start: int, end: int) -> int:
         while start < end:
             mid = (start + end) // 2
-            if self.data[mid] <= target: start = mid+1
+            if self.data[mid] <= target: start = mid + 1
             else: end = mid
         return start
 
     def bisect_left(target: int, start: int, end: int) -> int:
         while start < end:
             mid = (start + end) // 2
-            if self.data[mid] < target: start = mid+1
+            if self.data[mid] < target: start = mid + 1
             else: end = mid
         return start
