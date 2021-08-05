ccaadace79786f9828f1e1e5ba1b3f545f0112e5
Yuan-Yao Lou <yyloumike@gmail.com>
2021-08-05 15:39:22 +0800

    Add Solution (Python)

--- 0200-number-of-islands/solution.py
+++ 0200-number-of-islands/solution.py
@@ -33,28 +33,33 @@ class Solution:
                             
         return ans
         
-        '''
+    '''
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # (base case)
+        if len(grid) == 1 and len(grid[0]) == 1: return 1 if grid[0][0] == "1" else 0
+        
         # ==================================================
         #  DFS                                             =
         # ==================================================
-        # time  : O(nm)
-        # space : O(nm)
+        # time  : O(mn)
+        # space : O(mn)
+
+        island = 0
+        self.grid = grid
         
-        def explore(row, col):
-            grid[row][col] = 0
-            
-            if row > 0              and grid[row-1][col] == '1': explore(row-1, col)
-            if row < len(grid)-1    and grid[row+1][col] == '1': explore(row+1, col)
-            if col > 0              and grid[row][col-1] == '1': explore(row, col-1)
-            if col < len(grid[0])-1 and grid[row][col+1] == '1': explore(row, col+1)
-            
-        ans = 0
-            
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    ans += 1
-                    explore(i, j)
-                    
-        return ans
-        '''
+                if grid[i][j] == '1': 
+                    self.explore(i, j)
+                    island += 1
+        
+        return island
+        
+    def explore(self, y: int, x: int) -> None:
+        self.grid[y][x] = 0
+        
+        if y > 0                     and self.grid[y-1][x] == '1': self.explore(y-1, x)
+        if y < len(self.grid) - 1    and self.grid[y+1][x] == '1': self.explore(y+1, x)
+        if x > 0                     and self.grid[y][x-1] == '1': self.explore(y, x-1)
+        if x < len(self.grid[0]) - 1 and self.grid[y][x+1] == '1': self.explore(y, x+1)
+    '''
