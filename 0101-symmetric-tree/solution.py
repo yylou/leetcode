c667bc207a3d97479268bf442c64ea52fd00de60
Yuan-Yao Lou <yyloumike@gmail.com>
2021-08-23 16:17:02 -0700

    Add Solution (Python + Java)

--- 0101-symmetric-tree/solution.py
+++ 0101-symmetric-tree/solution.py
@@ -23,3 +23,21 @@ class Solution:
             stack.append((node1.right, node2.left))
             
         return True
+
+        '''
+        # ==================================================
+        #  Tree                               (Recursive)  =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+        
+        def isMirror(leftNode, rightNode):
+            if not leftNode and not rightNode: return True
+            if not leftNode or  not rightNode: return False
+            
+            return (leftNode.val == rightNode.val)              \
+                and isMirror(leftNode.left,  rightNode.right)   \
+                and isMirror(leftNode.right, rightNode.left)
+        
+        return isMirror(root.left, root.right)
+        '''
