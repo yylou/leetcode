053ce8bebe4498561fc45dfa2287b5e67bf7390d
Yuan-Yao Lou <yyloumike@gmail.com>
2021-09-05 22:18:44 -0400

    Add Solution (Python)

--- 0022-generate-parentheses/solution.py
+++ 0022-generate-parentheses/solution.py
@@ -1,26 +1,50 @@
-class Solution(object):
-    def insertParenthese(self, string):
-        ret_value = set()
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        # ==================================================
+        #  Dynamic Programming                             =
+        # ==================================================
+    
+        dp = {1: set(['()']), 2: set(['(())', '()()'])}
+        
+        # (base case)
+        if n == 1 or n == 2: return dp[n]
+        
+        for i in range(3, n+1):
+            # pattern 1: outer parenthese + subproblem with length - 1
+            dp[i] = set(['(' + x + ')' for x in dp[i-1]])
+            
+            # pattern 2: dp[i] is formed by dp[j] + dp[i-j]
+            for j in range(1, i):
+                # perform set union on combinations of elements in dp[j] and dp[i-j]
+                dp[i] |= set([x + y for x in dp[j] for y in dp[i-j]])
+        
+        return list(dp[n])
 
-        for i in xrange( len(string) ) : ret_value.add( string[:i] + '()' + string[i:] )
-
-        return ret_value
-
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-
-        ret_value = ['()']
-
-        for i in xrange(n-1) :
+    '''
+    def generateParenthesis(self, n: int) -> List[str]:
+        # (base case)
+        if n == 1: return ['()']
+        
+        # ==================================================
+        #  String + Set                                    =
+        # ==================================================
+        
+        ans = ['()']
+        
+        for _ in range(n-1):
             result = set()
 
-            ##  insert '()' in each index of string
-            ##  then using SET JOIN to combine without duplicate items
-            for string in ret_value : result |= self.insertParenthese( string )
+            # insert '()' in each index of string
+            # then using SET JOIN to combine without duplicate items
+            for string in ans : result |= self.insertParenthesis( string )
 
-            ret_value = result
+            ans = result
 
-        return ret_value
+        return ans
+    
+    def insertParenthesis(self, string) -> set:        
+        ret = set()
+        for i in range(len(string)) : ret.add(string[:i] + '()' + string[i:])
+        return ret
+    '''
