b993fc7906311c9d7abb747b9c08bae9db43bb32
Yuan-Yao Lou <yyloumike@gmail.com>
2021-09-08 23:43:10 -0400

    Add Solution (Python + Java)

--- 0026-remove-duplicates-from-sorted-array/solution.py
+++ 0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,6 +1,6 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        #  (base case)
+        # (base case)
         if len(nums) == 0 or len(nums) == 1: return len(nums)
         
         # ==================================================
@@ -14,7 +14,7 @@ class Solution:
         while moveP < len(nums):
             num = nums[moveP]
             
-            #  (meet number different from previous placed number)
+            # meet number different from previous placed number
             if placeP < 1 or num != nums[placeP - 1]:
                 nums[placeP] = num
                 placeP += 1
