4842cc7ba287a34a9d743f3e693cdb1217366989
Yuan-Yao Lou <yyloumike@gmail.com>
2021-12-01 13:55:46 -0500

    Update solution.py

--- 0283-move-zeroes/solution.py
+++ 0283-move-zeroes/solution.py
@@ -1,19 +1,19 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        #  (base case)
+        # (base case)
         if len(nums) == 1: return
-        
+
         # ==================================================
         #  Array + Two Pointer                             =
         # ==================================================
         # time  : O(n)
         # space : O(1)
-        
+
         placeP, moveP = 0, 0
-        
+
         while moveP < len(nums):
-            if nums[moveP] != 0: 
-                nums[placeP], nums[moveP] = nums[moveP], nums[placeP]    
+            if nums[moveP] != 0:
+                nums[placeP], nums[moveP] = nums[moveP], nums[placeP]
                 placeP += 1
-            
+
             moveP += 1
