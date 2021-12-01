a6ad8410e3da1cb1d738e84d38ff2996e2535476
Yuan-Yao Lou <yyloumike@gmail.com>
2021-12-01 16:06:27 -0500

    Add Solution (Python + Java)

--- 0001-2-sum/solution.java
+++ 0001-2-sum/solution.java
@@ -7,7 +7,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         
-        for(int i = 0; i < nums.length; i++) {
+        for(int i=0 ; i<nums.length ; i++) {
             int remain = target - nums[i];
             if(map.containsKey(remain)) return new int[] {i, map.get(remain)};
             
--- 0001-2-sum/solution.py
+++ 0001-2-sum/solution.py
@@ -7,12 +7,12 @@ class Solution:
         # ==================================================
         # time  : O(n)
         # space : O(n)
-        
-        for i in range( len(nums) ):
+
+        for i in range(len(nums)):
             remain = target - nums[i]
-            
+
             if remain in hashTable:
                 return [i, hashTable[remain]]
-            
-            #:  Since there is only one exact solution, it has no problem to overwrite the record
+
+            # We could overwrite since there is only one solution
             hashTable[nums[i]] = i
