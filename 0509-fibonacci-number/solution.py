92631f56aafbbf51d971c1f5b8ae1ee002f0b966
Yuan-Yao Lou <yyloumike@gmail.com>
2021-12-01 15:58:13 -0500

    Update solution.py

--- 0509-fibonacci-number/solution.py
+++ 0509-fibonacci-number/solution.py
@@ -2,18 +2,15 @@ class Solution:
     def fib(self, n: int) -> int:
         # (base case)
         if n == 0 or n == 1: return n
-        
+
         # ==================================================
         #  Dynamic Programming                             =
         # ==================================================
         # time  : O(n)
         # space : O(1)
-        
-        first, second = 0, 1
-        
+
+        n1, n2 = 0, 1
         for i in range(n - 1):
-            tmp = first
-            first = second
-            second += tmp
-            
-        return second
+            n1, n2 = n2, n1 + n2
+
+        return n2
