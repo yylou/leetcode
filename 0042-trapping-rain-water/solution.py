0b236fcbdd369bc4272615a27e1d27a42cd552a7
Yuan-Yao Lou <yyloumike@gmail.com>
2021-12-04 15:34:47 -0500

    Update solution.py

--- 0042-trapping-rain-water/solution.py
+++ 0042-trapping-rain-water/solution.py
@@ -1,71 +1,109 @@
-class Solution(object):
-    def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-
-        ##  (edge case) length < 3 (cannot form a gap)
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # (base case)
         if len(height) < 3: return 0
 
+        l, r = 0, len(height) - 1
+        while l < len(height) and height[l] == 0: l += 1
+        while r >= 0 and height[r] == 0: r -= 1
+        if r - l + 1 < 3: return 0
 
-        ##  Solution (1) two-pointer (both start from the begining)
-        def calculateArea( height ):
-            slowP, fastP = 0, 0
-
-            ##  (a) Find the first non-Zero element
-            while True:
-                if height[slowP] != 0: break
-                slowP += 1
-            fastP = slowP+1
-
-            area = 0
-            tmpSum = height[slowP]
-            tmpMin = height[slowP]
-
-            ##  (b) Using two pointers to visit the rest of array
-            while fastP < len(height):
-                ##  move fast-pointer until meet the HIGHER bar
-                if height[fastP] < height[slowP]:
-                    if height[fastP] < tmpMin: tmpMin = height[fastP]
-                    tmpSum += height[fastP]
-                    fastP += 1
-
-                else:
-                    ##  find gap, calcuate the area
-                    if tmpMin < height[slowP]:
-                        gap = fastP - slowP
-                        area += gap * height[slowP] - tmpSum
-
-                    slowP = fastP
-                    fastP += 1
-
-                    tmpSum = height[slowP]
-                    tmpMin = height[slowP]
-
-            return slowP, tmpMin, area
-
-        ##  (c) remaining part handling
-        slowP, tmpMin, area = calculateArea( height )
-
-        if slowP != len(height)-1 and tmpMin < height[slowP]:
-            area += calculateArea( height[slowP:][::-1] )[2]
-
-        return area
-
-
-        # ========================================================= #
-
-
-        ##  Solution (2) find the each GAP and accumulate
-        ##  - time complexity: O(n^2)
-        ##  - space complexity: O(1)
         area = 0
 
-        for i in range( len(height) ):
-            leftMax = max( height[0:i+1] )
-            rightMax = max( height[i:len(height)] )
+        # ==================================================
+        #  Two pointers                                    =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
 
-            area += min( leftMax, rightMax ) - height[i]
+        maxL, maxR = height[l], height[r]
+
+        while l < r:
+            if height[l] > height[r]:
+                if height[r] > maxR: maxR = height[r]
+                elif not height[r] == maxR: area += maxR - height[r]
+                r -= 1
+
+            else:
+                if height[l] > maxL: maxL = height[l]
+                elif not height[l] == maxL: area += maxL - height[l]
+                l += 1
 
         return area
+
+        """
+            |           |
+        |ooo|           |ooo|
+        ||o||           ||o||
+
+
+             |
+        |oooo|
+        |oo|o|                |
+        ||o|||            |ooo||o|
+        ||o|||          |o||o||||||
+        """
+
+        '''
+        # ==================================================
+        #  Dynamic Programming                             =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+
+        n, recL, recR = r - l + 1, float('-inf'), float('-inf')
+        maxL, maxR = [0] * len(height), [0] * len(height)
+
+        for i in range(l, r+1):
+            recL = max(recL, height[i])
+            maxL[i] = recL
+
+        for i in range(r, l-1, -1):
+            recR = max(recR, height[i])
+            maxR[i] = recR
+
+        for i in range(l, r+1):
+            area += min(maxL[i], maxR[i]) - height[i]
+
+        return area
+        '''
+
+        '''
+        # ==================================================
+        #  Monotonic Stack                                 =
+        # ==================================================
+        # time  : O(n)
+        # space : O(n)
+
+        stack = []
+        for i in range(l, r+1):
+            cur = height[i]
+
+            while stack and stack[-1][1] < cur:
+                index, val = stack.pop()
+                if not stack: break
+
+                depth = min(cur, stack[-1][1]) - val
+                gap   = i - stack[-1][0] - 1
+                area += depth * gap
+
+            stack.append((i, cur))
+
+        return area
+        '''
+
+        '''
+        # ==================================================
+        #  MAX for each iteration                          =
+        # ==================================================
+        # time  : O(n^2)
+        # space : O(1)
+
+        for i in range(l, len(height)):
+            leftMax  = max(height[0:i+1])
+            rightMax = max(height[i:len(height)])
+
+            area += min(leftMax, rightMax) - height[i]
+
+        return area
+        '''
