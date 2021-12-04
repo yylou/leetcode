cdd47bcd325175a458ce5c8b3e90068658db60f3
Yuan-Yao Lou <yyloumike@gmail.com>
2021-12-04 15:35:06 -0500

    Update solution.py

--- 0011-container-with-most-water/solution.py
+++ 0011-container-with-most-water/solution.py
@@ -2,24 +2,23 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         # (base case)
         if len( height ) == 2: return min( height )
-        
+
         # ==================================================
         #  Array + Two Pointer                             =
         # ==================================================
         # time  : O(n)
         # space : O(1)
-        
+
         area = 0
         l, r = 0, len(height) - 1
-        
+
         # start from both-side (due to the LARGEST gap)
         # move the pointer with SHORTER height
         while r > l:
             tmp = min(height[l], height[r]) * (r - l)
-            
+            if tmp > area: area = tmp
+
             if height[r] > height[l]: l += 1
             else: r -= 1
-                
-            if tmp > area: area = tmp
-                
+
         return area
