e2ae945c440a14345999b5fddacebfcc9c8d9c98
Yuan-Yao Lou <yyloumike@gmail.com>
2021-12-05 14:51:05 -0500

    Update solution.py

--- 0141-linked-list-cycle/solution.py
+++ 0141-linked-list-cycle/solution.py
@@ -18,31 +18,3 @@ class Solution:
             if slowP == fastP: return True
 
         return False
-
-'''
-Java Solution
-==================================================================================================
-class Solution {
-    /**
-     * @time  : O(n)
-     * @space : O(1)
-     */
-
-    public boolean hasCycle(ListNode head) {
-        /* base case */
-        if(head == null || head.next == null) return false;
-        if(head.next.next == head) return true;
-
-        ListNode slowP = head, fastP = head;
-
-        while(fastP != null && fastP.next != null) {
-            slowP = slowP.next;
-            fastP = fastP.next.next;
-            if(slowP == fastP) return true;
-        }
-
-        return false;
-    }
-}
-==================================================================================================
-'''
