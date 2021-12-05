2bd5ef5bb344f1c1707f36665dc1ca2812102d4c
Yuan-Yao Lou <yyloumike@gmail.com>
2021-12-05 15:36:54 -0500

    Add Solution (Python + Java)

--- 0142-linked-list-cycle-ii/solution.py
+++ 0142-linked-list-cycle-ii/solution.py
@@ -1,64 +1,29 @@
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
-        #  (base case)
+        # (base case)
         if not head or not head.next: return None
         if head == head.next.next: return head
-        
+
         # ==================================================
-        #  Linked List + Two Pointer  (Floyd's algorithm)  =
+        #  Linked List + Two Pointer (Floyd's algorithm)   =
         # ==================================================
         # time  : O(n)
         # space : O(1)
-        
+
         slowP, fastP = head, head
-        
-        #  (find the intersection)
+
+        # Find the intersection
         while fastP and fastP.next:
             slowP = slowP.next
             fastP = fastP.next.next
             if slowP == fastP: break
-                
+
         if slowP != fastP: return None
-        
-        #  (find the cycle entrance)
+
+        # Find the cycle entrance
         slowP = head
         while slowP != fastP:
             slowP = slowP.next
             fastP = fastP.next
-        
+
         return slowP
-    
-'''
-Java Solution
-==================================================================================================
-class Solution {
-    /**
-     * @time  : O(n)
-     * @space : O(1)
-     */
-    
-    public ListNode detectCycle(ListNode head) {
-        /* case case */
-        if(head == null || head.next == null) return null;
-        if(head == head.next.next) return head;
-        
-        ListNode slowP = head, fastP = head;
-        while(fastP != null && fastP.next != null) {
-            slowP = slowP.next;
-            fastP = fastP.next.next;
-            if(slowP == fastP) break;
-        }
-        
-        if(slowP != fastP) return null;
-        
-        slowP = head;
-        while(slowP != fastP) {
-            slowP = slowP.next;
-            fastP = fastP.next;
-        }
-        
-        return slowP;
-    }
-}
-==================================================================================================
-'''
