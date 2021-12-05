5e36d8145f85ea1b5c59f5a289b44e32beb45abb
Yuan-Yao Lou <yyloumike@gmail.com>
2021-12-05 17:13:35 -0500

    Update solution.py

--- 0287-find-the-duplicate-number/solution.py
+++ 0287-find-the-duplicate-number/solution.py
@@ -1,54 +1,24 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        
+
         # ==================================================
-        #  Array + Two Pointer                             =
+        #  Two Pointer                                     =
         # ==================================================
         # time  : O(n)
         # space : O(1)
-        
+
         slowP, fastP = nums[0], nums[0]
-        
-        #  (find the intersection)
+
+        # Find the intersection
         while True:
             slowP = nums[slowP]
             fastP = nums[nums[fastP]]
             if slowP == fastP: break
-                
-        #  (find the entrance of the cycle)
+
+        # Find the entrance of the cycle
         slowP = nums[0]
         while slowP != fastP:
             slowP = nums[slowP]
             fastP = nums[fastP]
-            
+
         return slowP
-    
-'''
-Java Solution
-==================================================================================================
-class Solution {
-    /**
-     * @time  : O(n)
-     * @space : O(1)
-     */
-     
-    public int findDuplicate(int[] nums) {
-        int slowP = nums[0], fastP = nums[0];
-        
-        while(true) {
-            slowP = nums[slowP];
-            fastP = nums[nums[fastP]];
-            if(slowP == fastP) break;
-        }
-        
-        slowP = nums[0];
-        while(slowP != fastP) {
-            slowP = nums[slowP];
-            fastP = nums[fastP];
-        }
-        
-        return slowP;
-    }
-}
-==================================================================================================
-'''
