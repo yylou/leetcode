f54a6ae6c56f80cf8e36eca137bdafc0fdea97fa
Yuan-Yao Lou <yyloumike@gmail.com>
2021-12-05 00:10:43 -0500

    Update solution.py

--- 0141-linked-list-cycle/solution.py
+++ 0141-linked-list-cycle/solution.py
@@ -1,24 +1,24 @@
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        #  (base case)
+        # (base case)
         if not head or not head.next: return False
         if head.next.next == head: return True
-        
+
         # ==================================================
         #  Linked List + Two Pointer                       =
         # ==================================================
         # time  : O(n)
         # space : O(1)
-        
+
         slowP, fastP = head, head
-        
+
         while fastP and fastP.next:
             slowP = slowP.next
             fastP = fastP.next.next
             if slowP == fastP: return True
-        
+
         return False
-        
+
 '''
 Java Solution
 ==================================================================================================
@@ -27,20 +27,20 @@ class Solution {
      * @time  : O(n)
      * @space : O(1)
      */
-     
+
     public boolean hasCycle(ListNode head) {
         /* base case */
         if(head == null || head.next == null) return false;
         if(head.next.next == head) return true;
-        
+
         ListNode slowP = head, fastP = head;
-        
+
         while(fastP != null && fastP.next != null) {
             slowP = slowP.next;
             fastP = fastP.next.next;
             if(slowP == fastP) return true;
         }
-        
+
         return false;
     }
 }
