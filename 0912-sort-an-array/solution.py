bcaf1df3bd1e7642129e39c78b2ca78e28e2cc44
Yuan-Yao Lou <yyloumike@gmail.com>
2021-12-05 14:58:03 -0500

    Update solution.py

--- 0912-sort-an-array/solution.py
+++ 0912-sort-an-array/solution.py
@@ -3,15 +3,15 @@ class Solution:
         # return self.quickSort(nums)
         # return self.quickSort1(nums)
         return self.mergeSort(nums)
-        
-    
+
+
     def mergeSort(self, nums: List[int]) -> List[int]:
         # ==================================================
-        #  Array + Quick Sort                              =
+        #  Array + Merge Sort                              =
         # ==================================================
         # time  : O(nlog(n))
         # space : O(n)
-        
+
         def merge(left: List[int], right: List[int]) -> List[int]:
             ret, p1, p2 = [], 0, 0
             while p1 < len(left) and p2 < len(right):
@@ -23,26 +23,26 @@ class Solution:
                     p2 += 1
             ret += left[p1:] or right[p2:]
             return ret
-        
+
         if len(nums) == 1: return nums
-        
+
         left, right = self.mergeSort(nums[:len(nums)//2]), self.mergeSort(nums[len(nums)//2:])
         return merge(left, right)
-    
+
     def quickSort(self, nums: List[int]) -> List[int]:
         # ==================================================
         #  Array + Quick Sort                              =
         # ==================================================
         # time  : O(nlog(n))
         # space : O(nlog(n))
-        
+
         def sort(nums: List[int], left: int, right: int) -> None:
             if right == left or right - left == 1: return
-            
+
             pivot = partition(nums, left, right - 1)
             sort(nums, left, pivot)
             sort(nums, pivot + 1, right)
-            
+
         def partition(nums: List[int], left: int, right: int) -> int:
             randomNum = (left + right) // 2
             pivot = right
@@ -54,25 +54,25 @@ class Solution:
                     placeP += 1
             nums[pivot], nums[placeP] = nums[placeP], nums[pivot]
             return placeP
-        
+
         sort(nums, 0, len(nums))
         return nums
-    
+
     def quickSort1(self, nums: List[int]) -> List[int]:
         # (base case)
         if not nums or len(nums) == 1: return nums
-        
+
         # ==================================================
         #  Array + Quick Sort                              =
         # ==================================================
         # time  : O(nlog(n))
         # space : O(nlog(n))
-        
+
         pivot = nums[len(nums) // 2]
         lt, eq, lg = list(), list(), list()
         for num in nums:
             if num == pivot: eq += [num]
             elif num > pivot: lg += [num]
             else: lt += [num]
-                
+
         return self.quickSort(lt) + eq + self.quickSort(lg)
