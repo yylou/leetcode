f9da8851ac6656bd1901f06744cd59ee618f0615
Yuan-Yao Lou <yyloumike@gmail.com>
2022-02-28 17:52:15 -0500

    Update solution.py

--- 0023-merge-k-sorted-lists/solution.py
+++ 0023-merge-k-sorted-lists/solution.py
@@ -3,38 +3,53 @@ class Solution:
         # (base case)
         if len(lists) == 0: return None
         if len(lists) == 1: return lists[0]
-        
+
         # ==================================================
         #  Linked List + Sort                              =
         # ==================================================
         # time  : O(nlogk)
         # space : O(1)
-        # 
+        #
         # n is the total number of nodes in two linked lists
         # k is the number of linked lists
-        
+
         size = 1
         while size < len(lists):
             for i in range(0, len(lists) - size, size * 2):
-                lists[i] = self.merge2Lists(lists[i], lists[i+size])
+                lists[i] = self._merge(lists[i], lists[i+size])
             size *= 2
-            
+
         return lists[0]
-        
-    def merge2Lists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        ret = ListNode(0)
-        cur = ret
-        
+
+        """
+                                        length = 6
+            [l0, l1, l2, l3, l4, l5]    size = 1, interval = 2, upper = 5
+             ^----^  ^----^  ^----^
+
+            [l0,     l2,     l4    ]    size = 2, interval = 4, upper = 4
+             ^--------^
+
+            [l0,             l4    ]    size = 4, interval = 8, upper = 2
+             ^----------------^
+
+                                        length = 5
+            [l0, l1, l2, l3, l4]        size = 1, interval = 2, upper = 5
+             ^----^  ^----^
+
+            [l0,     l2,     l4]        size = 2, interval = 4, upper = 4
+             ^--------^
+
+            [l0,             l4]        size = 4, interval = 8, upper = 2
+             ^----------------^
+        """
+
+    def _merge(self, l1, l2):
+        head = cur = ListNode(0)
+
         while l1 and l2:
-            if l1.val < l2.val:
-                cur.next = ListNode(l1.val)
-                cur = cur.next
-                l1 = l1.next
-            else:
-                cur.next = ListNode(l2.val)
-                cur = cur.next
-                l2 = l2.next
-                
+            if l1.val <= l2.val: cur.next, l1 = l1, l1.next
+            else: cur.next, l2 = l2, l2.next
+            cur = cur.next
+
         cur.next = l1 or l2
-                
-        return ret.next
+        return head.next
