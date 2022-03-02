1ed470cd28e73f51ccc5ef6d2a9c1b98ea9ab30e
Yuan-Yao Lou <yyloumike@gmail.com>
2022-03-01 21:31:49 -0500

    Update solution.py

--- 0680-valid-palindrome-ii/solution.py
+++ 0680-valid-palindrome-ii/solution.py
@@ -1,53 +1,25 @@
-class Solution(object):
-    def validPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        # (base case)
+        if len(s) == 1 or len(s) == 2: return True
 
-        ##  (edge case) length == 1 or 2
-        n = len(s)
-        if n == 1 or n == 2: return True
+        # ==================================================
+        #  Two Pointer                                     =
+        # ==================================================
+        # time  : O(n)
+        # space : O(1)
 
+        return self._validPalindrome(s, True)
 
-        ##  Solution (1) check by reversing string
-        left, right = 0, n-1
-        while left <= right:
-            if s[left] != s[right]:
-                ##  skip one char in left side and right side, then reverse string to check
-                if s[left+1:right+1] == s[left+1:right+1][::-1]: return True
-                if s[left:right] == s[left:right][::-1]: return True
+    def _validPalindrome(self, s: str, skip: bool) -> bool:
+        # (base case)
+        if len(s) == 1: return True
 
-                return False
+        l, r = 0, len(s) - 1
+        while l < r:
+            if   s[l] != s[r] and     skip: return self._validPalindrome(s[l+1:r+1], False) or self._validPalindrome(s[l:r], False)
+            elif s[l] != s[r] and not skip: return False
 
-            else:
-                left  += 1
-                right -= 1
+            l, r = l+1, r-1
 
         return True
-
-
-        # ========================================================================================= #
-
-
-        ##  Solution (2) check by two check process branches
-        def check( s, skip ):
-            left, right = 0, len(s)-1
-
-            while left <= right:
-                if s[left] != s[right]:
-                    if skip:
-                        if check( s[left+1:right+1], False ) or check( s[left:right], False ):
-                            return True
-                        else:
-                            return False
-                    else:
-                        return False
-
-                else:
-                    left  += 1
-                    right -= 1
-
-            return True
-
-        return check( s, True )
