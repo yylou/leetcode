a381f1486416e27412885bdef135e6b3fc65297b
Yuan-Yao Lou <yyloumike@gmail.com>
2022-03-15 15:10:49 -0400

    Update solution.py

--- 0001-2-sum/solution.py
+++ 0001-2-sum/solution.py
@@ -1,6 +1,5 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashTable = dict()
 
         # ==================================================
         #  Array + Hash Table                              =
@@ -8,11 +7,12 @@ class Solution:
         # time  : O(n)
         # space : O(n)
 
+        table = dict()
         for i in range(len(nums)):
             remain = target - nums[i]
 
-            if remain in hashTable:
-                return [i, hashTable[remain]]
+            if remain in table:
+                return [i, table[remain]]
 
             # We could overwrite since there is only one solution
-            hashTable[nums[i]] = i
+            table[nums[i]] = i
