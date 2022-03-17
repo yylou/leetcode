436fd61313d15254c5e1921638c307015025d283
Yuan-Yao Lou <yyloumike@gmail.com>
2022-03-17 18:11:44 -0400

    Update solution.py

--- 0066-plus-one/solution.py
+++ 0066-plus-one/solution.py
@@ -1,50 +1,26 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        #: (edge case)
-        if len(digits) == 1: return [1, 0] if digits[0] == 9 else [digits[0] + 1]
-        
+
+        """
         # ==================================================
-        #  Array + Math                                    =
+        #  [Array] Math                                    =
         # ==================================================
         # time  : O(n)
-        # space : O(1) 
-        
-        carry = 1
-        for i in range( len(digits)-1, -1, -1 ):
-            digits[i] += carry
-            carry = digits[i] // 10
-            
-            #: if carry == 0, return; otherwise, keep tracking and assign 0 to current element
-            if carry: digits[i] = 0
-            else: return digits
-            
-        return [1] + digits
-    
-'''
-Java Solution
-==================================================================================================
-class Solution {
-    /**
-     * @time  : O(n)
-     * @space : O(n)
-     */
+        # space : O(1)
+        """
 
-    public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        
-        for( int i=n-1 ; i>-1 ; i-- ){
-            if( digits[i] == 9 ){
-                digits[i] = 0;
-            } else{
-                digits[i]++;
-                return digits;
-            }
-        }
-        
-        digits = new int[n+1];
-        digits[0] = 1;
-        return digits;
-    }
-}
-==================================================================================================
-'''
+        # (base case) Only one digit
+        if len(digits) == 1: return [1, 0] if digits[0] == 9 else [digits[0] + 1]
+
+        carry = 1
+        for i in range(len(digits) - 1, -1, -1):
+            total = digits[i] + carry
+
+            carry = total // 10
+            digits[i] = total % 10
+
+            # No need to further progagate
+            if not carry: return digits
+
+        if carry: return [1] + digits
+        return digits
