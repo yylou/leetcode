45e9fa3609fdda01ef41fe8f8651dfe0a6f1ff7b
Yuan-Yao Lou <yyloumike@gmail.com>
2022-03-19 15:52:26 -0400

    Update solution.py

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/solution.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/solution.py
@@ -1,20 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        #  (base case)
-        if len(prices) == 0 or len(prices) == 1: return 0
 
+        """
         # ==================================================
-        #  Array + Dynamic Programming              (FSM)  =
+        #  [Array] DP                                      =
         # ==================================================
         # time  : O(n)
         # space : O(1)
-        
-        hold, noHold = float('-inf'), 0
-        
+        """
+
+        # (base case)
+        if len(prices) == 1: return 0
+
+        hold, sell = float('-inf'), 0
+
         for price in prices:
-            preHold, preNoHold = hold, noHold
-            
-            hold   = max(preHold,   preNoHold - price)
-            noHold = max(preNoHold, preHold   + price - fee)
-            
-        return noHold
+            preHold, preSell = hold, sell
+
+            hold = max(preHold, preSell - price)
+            sell = max(preSell, preHold + price - fee)
+
+        return sell
