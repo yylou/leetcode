bdf4a1c2c157daa5d42ff914bf29e2656b63ca0b
Yuan-Yao Lou <yyloumike@gmail.com>
2022-03-19 15:53:00 -0400

    Update solution.py

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.py
@@ -1,27 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #  (base case)
-        if len(prices) == 1: return 0
-        
+
+        """
         # ==================================================
-        #  Array + Dynamic Programming              (FSM)  =
+        #  [Array] DP                                      =
         # ==================================================
         # time  : O(n)
         # space : O(1)
+        """
 
-        hold, noHold = float('-inf'), 0
-        
-        #  after selling stock, you cannot buy stock on the next day = NO HOLD state
-        preDaySell = 0
-        
+        # (base case)
+        if len(prices) == 1: return 0
+
+        hold, sell = float('-inf'), 0
+        rest = 0
+
         for price in prices:
-            preHold, preNoHold = hold, noHold
-            
-            #  due to cooldown, cannot use preNoHold's profit to buy stock, use preDaySell
-            hold   = max(preHold,   preDaySell - price)
-            noHold = max(preNoHold, preHold    + price)
-            
-            #  record at the end to have an one-day gap (still 0 after 1st iteration)
-            preDaySell = preNoHold
-            
-        return noHold
+            preHold, preSell = hold, sell
+            preRest = rest
+
+            hold = max(preHold, preRest - price)
+            sell = max(preSell, preHold + price)
+            rest = max(preRest, preSell)
+
+        return max(sell, rest)
